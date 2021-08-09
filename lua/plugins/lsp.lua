dc7420c819a5a6ce2d4c90e1bb1cd1898222f856
Michael <michaeljohannpeter@gmail.com>
2021-08-09 17:52:32 +0200

    Add terraformls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,7 +43,7 @@ go get golang.org/x/tools/gopls
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "gopls", "bashls", "dockerls", "yamlls"}
+local servers = { "gopls", "bashls", "dockerls", "terraformls", "yamlls"}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
