783790b7cbcf79add6a04e40b1a50d5e3be65e5c
Michael <michaeljohannpeter@gmail.com>
2021-09-05 15:30:55 +0200

    update nightfox options

--- lua/options.lua
+++ lua/options.lua
@@ -86,6 +86,25 @@ local g = vim.g
 -- vim.cmd[[colorscheme tokyonight]]
 
 -- nightfox theme
-g.nightfox_style = "nightfox"
-require('nightfox').set()
+local nightfox = require('nightfox')
+-- This function set the configuration of nightfox. If a value is not passed in the setup function
+-- it will be taken from the default configuration above
+nightfox.setup({
+  fox = "nightfox", -- change the colorscheme to use nordfox
+  styles = {
+    comments = "italic", -- change style of comments to be italic
+    keywords = "bold", -- change style of keywords to be bold
+    functions = "italic,bold" -- styles can be a comma separated list
+  },
+  colors = {
+    red = "#FF000", -- Override the red color for MAX POWER
+    bg_alt = "#000000",
+  },
+  hlgroups = {
+    TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
+    LspCodeLens = { bg = "#000000", style = "italic" },
+  }
+})
+-- Load the configuration set above and apply the colorscheme
+nightfox.load()
 
