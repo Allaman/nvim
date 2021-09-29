1f203c05565d0ecdfa95dd9821f5e7028ecd9e2b
Michael <michaeljohannpeter@gmail.com>
2021-09-29 23:15:35 +0200

    add jedi lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,7 +35,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "gopls", "bashls", "dockerls", "terraformls", "tsserver", "texlab", "yamlls"}
+local servers = { "gopls", "bashls", "jedi_language_server", "dockerls", "terraformls", "tsserver", "texlab", "yamlls"}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
