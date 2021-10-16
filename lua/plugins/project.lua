6f02053859b288f512627f382bb3fbcc1df6d8fb
Michael <michaeljohannpeter@gmail.com>
2021-10-16 22:02:16 +0200

    Remove Makefile as project root indicator

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,6 +1,6 @@
 local g = vim.g
 require("project_nvim").setup {
-  patterns = { ".git", "Makefile", "package.json", ".terraform", "go.mod", "requirements.yml" },
+  patterns = { ".git", "package.json", ".terraform", "go.mod", "requirements.yml" },
   -- detection_methods = { "lsp", "pattern" },
   detection_methods = { "pattern" },
 }
