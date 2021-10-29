a901f3b4be220ad8e9fb161b4519482e2ba2927e
Michael <michaeljohannpeter@gmail.com>
2021-10-29 19:57:14 +0200

    autopairs integration changed - remove obsolet code

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -110,15 +110,3 @@ cmp.setup.cmdline("/", {sources = {{name = "buffer"}}})
 cmp.setup.cmdline(":", {
     sources = cmp.config.sources({{name = "path"}}, {{name = "cmdline"}})
 })
-
--- work with auto-pairs
--- require("nvim-autopairs.completion.cmp").setup({
---     map_cr = true, --  map <CR> on insert mode
---     map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
---     auto_select = true, -- automatically select the first item
---     insert = false, -- use insert confirm behavior instead of replace
---     map_char = { -- modifies the function or method delimiter by filetypes
---         all = '(',
---         tex = '{'
---     }
--- })
