e9adb831a83e493f3fbcad7a80b066801675b34d
Michael <michaeljohannpeter@gmail.com>
2021-11-02 20:15:32 +0100

    autoformat

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -1,8 +1,7 @@
-require('Navigator').setup()
+require("Navigator").setup()
 local map = vim.api.nvim_set_keymap
 -- tmux navigation
-map('n', "<C-h>", "<cmd>lua require('Navigator').left()<CR>", options)
-map('n', "<C-k>", "<cmd>lua require('Navigator').up()<CR>", options)
-map('n', "<C-l>", "<cmd>lua require('Navigator').right()<CR>", options)
-map('n', "<C-j>", "<cmd>lua require('Navigator').down()<CR>", options)
-
+map("n", "<C-h>", "<cmd>lua require('Navigator').left()<CR>", default_options)
+map("n", "<C-k>", "<cmd>lua require('Navigator').up()<CR>", default_options)
+map("n", "<C-l>", "<cmd>lua require('Navigator').right()<CR>", default_options)
+map("n", "<C-j>", "<cmd>lua require('Navigator').down()<CR>", default_options)
