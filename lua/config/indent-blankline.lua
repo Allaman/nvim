15f111a63bac87e0965e96f9a2113af93ca80245
Michael <michaeljohannpeter@gmail.com>
2021-11-03 02:38:18 +0100

    add dedicated config file for indent-blankline

---
+++ lua/config/indent-blankline.lua
@@ -0,0 +1,20 @@
+require("indent_blankline").setup {
+    indentLine_enabled = 1,
+    char = "▏",
+    filetype_exclude = {
+        "startify", "dashboard", "dotooagenda", "log", "fugitive", "gitcommit",
+        "packer", "vimwiki", "markdown", "json", "txt", "vista", "help",
+        "todoist", "NvimTree", "peekaboo", "git", "TelescopePrompt", "undotree",
+        "flutterToolsOutline", "" -- for all buffers without a file type
+    },
+    buftype_exclude = {"terminal", "nofile"},
+    show_trailing_blankline_indent = false,
+    show_first_indent_level = true,
+    show_current_context = true,
+    char_list = {"|", "¦", "┆", "┊"},
+    space_char = " ",
+    context_patterns = {
+        "class", "function", "method", "block", "list_literal", "selector",
+        "^if", "^table", "if_statement", "while", "for"
+    }
+}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -101,27 +101,7 @@ return require("packer").startup(function(use)
 
     use {
         "lukas-reineke/indent-blankline.nvim",
-        branch = lua,
-        -- config = [[require("config/indent")]] TODO: I did not get to work an external config
-        setup = function()
-            vim.g.indent_blankline_char = "│"
-            vim.g.indent_blankline_show_first_indent_level = true
-            vim.g.indent_blankline_filetype_exclude = {
-                "startify", "dashboard", "dotooagenda", "log", "fugitive",
-                "gitcommit", "packer", "vimwiki", "markdown", "json", "txt",
-                "vista", "help", "todoist", "NvimTree", "peekaboo", "git",
-                "TelescopePrompt", "undotree", "flutterToolsOutline", "" -- for all buffers without a file type
-            }
-            vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
-            vim.g.indent_blankline_show_trailing_blankline_indent = false
-            vim.g.indent_blankline_show_current_context = true
-            vim.g.indent_blankline_char_list = {"|", "¦", "┆", "┊"}
-            vim.g.indent_blankline_space_char = " "
-            vim.g.indent_blankline_context_patterns = {
-                "class", "function", "method", "block", "list_literal",
-                "selector", "^if", "^table", "if_statement", "while", "for"
-            }
-        end
+        config = [[require("config/indent-blankline")]]
     }
 
     use {"akinsho/nvim-toggleterm.lua", config = get_config("toggleterm")}
