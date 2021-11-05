10952c1c0f803a705a99a61059a5fd5a0643651d
Michael <michaeljohannpeter@gmail.com>
2021-11-05 17:29:22 +0100

    add harpoon keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -117,6 +117,26 @@ wk.register({
             "Checkout commit(for current file)"
         }
     },
+    h = {
+        name = "Harpoon",
+        a = {"<cmd>lua require('harpoon.mark').add_file()<cr>", "Add file"},
+        u = {
+            "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
+            "Open Menu"
+        },
+        ["1"] = {
+            "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Open File 1"
+        },
+        ["2"] = {
+            "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Open File 2"
+        },
+        ["3"] = {
+            "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Open File 3"
+        },
+        ["4"] = {
+            "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Open File 4"
+        }
+    },
     j = {
         name = "jump",
         c = {"<cmd>lua require'hop'.hint_char2()<cr>", "2 Chars"},
