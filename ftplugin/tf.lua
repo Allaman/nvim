bd99227a0eaea312398340957ae42b94433d2a74
Michael <michaeljohannpeter@gmail.com>
2021-11-08 19:57:36 +0100

    autoformat

--- ftplugin/tf.lua
+++ ftplugin/tf.lua
@@ -1,11 +1,8 @@
 -- force tf files to filetype terraform
-vim.api.nvim_exec(
-  [[
+vim.api.nvim_exec([[
     augroup terraform
       autocmd!
       autocmd BufRead,BufNewFile *.tf set filetype=terraform
       autocmd BufWritePost *.tf FormatWrite
     augroup END
-  ]],
-  false
-)
+  ]], false)
