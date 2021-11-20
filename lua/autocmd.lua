d9593316e66d40e5db4ab5b5418099b4b9256f24
Michael <michaeljohannpeter@gmail.com>
2021-11-20 22:03:23 +0100

    autoformatting

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,20 +1,14 @@
 -- Remove all trailing whitespace on save
-vim.api.nvim_exec(
-  [[
-  augroup Formatting
+vim.api.nvim_exec([[
+  augroup TrimWhiteSpace
     au!
     autocmd BufWritePre * :%s/\s\+$//e
   augroup END
-  ]],
-  false
-)
+  ]], false)
 -- Prevent new line to also start with a comment
-vim.api.nvim_exec(
-  [[
-  augroup Commenting
+vim.api.nvim_exec([[
+  augroup NewLineComment
     au!
     au FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
   augroup END
-  ]],
-  false
-)
+  ]], false)
