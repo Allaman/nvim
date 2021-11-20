467b2781ba9c865dcc15674e037eda93c3729749
Michael <michaeljohannpeter@gmail.com>
2021-11-20 22:03:51 +0100

    show relativ filepath in lualine; autoformat

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,31 +1,31 @@
-require'lualine'.setup {
-  options = {
-    icons_enabled = true,
-    component_separators = {'', ''},
-    section_separators = {'', ''},
-    disabled_filetypes = {}
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff'},
-    lualine_c = {'filename'},
-    lualine_x = {
-      {"diagnostics", sources = {"nvim_lsp"}},
-      "encoding",
-      "fileformat",
-      "filetype"
+require"lualine".setup {
+    options = {
+        icons_enabled = true,
+        component_separators = {"", ""},
+        section_separators = {"", ""},
+        disabled_filetypes = {}
     },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {'nvim-tree', 'toggleterm', 'quickfix'}
+    sections = {
+        lualine_a = {"mode"},
+        lualine_b = {"branch", "diff"},
+        lualine_c = {
+            {"filename", file_status = true, path = 1, shorting_target = 40} -- show relativ path
+        },
+        lualine_x = {
+            {"diagnostics", sources = {"nvim_lsp"}}, "encoding", "fileformat",
+            "filetype"
+        },
+        lualine_y = {"progress"},
+        lualine_z = {"location"}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {"filename"},
+        lualine_x = {"location"},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    extensions = {"nvim-tree", "toggleterm", "quickfix"}
 }
