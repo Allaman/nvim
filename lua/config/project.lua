dd2cc81072eefa8fe9d64000bc4a9aef8eaa0524
Michael <michaeljohannpeter@gmail.com>
2021-12-01 19:55:53 +0100

    remove unused var; autoformat

--- lua/config/project.lua
+++ lua/config/project.lua
@@ -1,6 +1,7 @@
-local g = vim.g
 require("project_nvim").setup {
-  patterns = { ".git", "package.json", ".terraform", "go.mod", "requirements.yml" },
-  -- detection_methods = { "lsp", "pattern" },
-  detection_methods = { "pattern" },
+    patterns = {
+        ".git", "package.json", ".terraform", "go.mod", "requirements.yml"
+    },
+    -- detection_methods = { "lsp", "pattern" },
+    detection_methods = {"pattern"}
 }
