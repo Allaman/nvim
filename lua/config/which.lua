50f165087b57ef17c02eef2ff3b5b301ffbb6014
Michael <michaeljohannpeter@gmail.com>
2021-12-14 21:05:21 +0100

    move lsp keybindings to which config

--- lua/config/which.lua
+++ lua/config/which.lua
@@ -162,27 +162,45 @@ wk.register({
     },
     l = {
         name = "LSP",
-        a = {"<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action"},
-        d = {
-            "<cmd>Telescope lsp_document_diagnostics<cr>",
-            "Document Diagnostics"
+        A = {
+            "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>",
+            "Add Workspace Folder"
         },
-        w = {
-            "<cmd>Telescope lsp_workspace_diagnostics<cr>",
-            "Workspace Diagnostics"
+        D = {"<cmd>lua vim.lsp.buf.declaration()<cr>", "Go To Declaration"},
+        I = {
+            "<cmd>lua vim.lsp.buf.implementation()<cr>", "Show implementations"
         },
-        -- f = { "<cmd>silent FormatWrite<cr>", "Format" },
-        f = {"<cmd>lua vim.lsp.buf.formatting()<cr>", "Format"},
-        i = {"<cmd>LspInfo<cr>", "Info"},
-        j = {"<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic"},
-        k = {"<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic"},
-        q = {"<cmd>lua vim.diagnostic.set_loclist()<cr>", "Quickfix"},
-        r = {"<cmd>lua vim.lsp.buf.rename()<cr>", "Rename"},
-        s = {"<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols"},
+        K = {"<cmd>lua vim.lsp.buf.hover()<cr>", "Hover Commands"},
+        L = {
+            "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>",
+            "List Workspace Folders"
+        },
+        R = {"<cmd>lua vim.lsp.buf.rename()<cr>", "Rename"},
         S = {
             "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
             "Workspace Symbols"
-        }
+        },
+        W = {
+            "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>",
+            "Remove Workspace Folder"
+        },
+        a = {"<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action"},
+        d = {"<cmd>lua vim.lsp.buf.definition()<cr>", "Go To Definition"},
+        e = {"<cmd>Telescope diagnostics bufnr=0<cr>", "Document Diagnostics"},
+        f = {"<cmd>lua vim.lsp.buf.formatting()<cr>", "Format"},
+        i = {"<cmd><cr>", "Connected Language Servers"},
+        k = {"<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help"},
+        l = {
+            "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>",
+            "Line diagnostics"
+        },
+        n = {"<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic"},
+        p = {"<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic"},
+        q = {"<cmd>lua vim.diagnostic.set_loclist()<cr>", "Quickfix"},
+        r = {"<cmd>lua vim.lsp.buf.references()<cr>", "References"},
+        s = {"<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols"},
+        t = {"<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type Definition"},
+        w = {"<cmd>Telescope diagnostics<cr>", "Workspace Diagnostics"}
     },
     m = {
         name = "Misc",
