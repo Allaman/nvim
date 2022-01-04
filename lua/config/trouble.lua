9c3ce94c1bc118b56e263642440f89369bad03c1
Michael <michaeljohannpeter@gmail.com>
2022-01-04 23:25:28 +0100

    autoformatting

--- lua/config/trouble.lua
+++ lua/config/trouble.lua
@@ -13,9 +13,9 @@ require("trouble").setup {
         cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
         refresh = "r", -- manually refresh
         jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
-        open_split = { "<c-x>" }, -- open buffer in new split
-        open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
-        open_tab = { "<c-t>" }, -- open buffer in new tab
+        open_split = {"<c-x>"}, -- open buffer in new split
+        open_vsplit = {"<c-v>"}, -- open buffer in new vsplit
+        open_tab = {"<c-t>"}, -- open buffer in new tab
         jump_close = {"o"}, -- jump to the diagnostic and close the list
         toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
         toggle_preview = "P", -- toggle auto_preview
