b3787c20aa97a911e5c79c6b79b79f328c8b77fe
Michael <michaeljohannpeter@gmail.com>
2022-01-13 21:33:07 +0100

    add escape pair function/mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -46,3 +46,24 @@ map("n", "*", "<cmd>lua require'starlite'.star()<CR>", default_options)
 map("n", "g*", "<cmd>lua require'starlite'.g_star()<CR>", default_options)
 map("n", "#", "<cmd>lua require'starlite'.hash()<CR>", default_options)
 map("n", "g#", "<cmd>lua require'starlite'.g_hash()<CR>", default_options)
+function EscapePair()
+    local closers = {")", "]", "}", ">", "'", "\"", "`", ","}
+    local line = vim.api.nvim_get_current_line()
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local after = line:sub(col + 1, -1)
+    local closer_col = #after + 1
+    local closer_i = nil
+    for i, closer in ipairs(closers) do
+        local cur_index, _ = after:find(closer)
+        if cur_index and (cur_index < closer_col) then
+            closer_col = cur_index
+            closer_i = i
+        end
+    end
+    if closer_i then
+        vim.api.nvim_win_set_cursor(0, {row, col + closer_col})
+    else
+        vim.api.nvim_win_set_cursor(0, {row, col + 1})
+    end
+end
+map("i", "<C-l>", "<cmd>lua EscapePair()<CR>", default_options)
