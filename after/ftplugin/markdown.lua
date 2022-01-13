ff841cc8f7b65ca7e92b03bd74e29a359fcdd8aa
Michael <michaeljohannpeter@gmail.com>
2022-01-13 21:30:43 +0100

    refactor ftplugin

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+vim.opt_local.spell = true
+vim.opt_local.wrap = true
+
+-- match and highlight hyperlinks
+vim.fn.matchadd("matchURL", [[http[s]\?:\/\/[[:alnum:]%\/_#.-]*]])
+vim.cmd "hi matchURL guifg=DodgerBlue"
--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -1,4 +1,3 @@
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
-
