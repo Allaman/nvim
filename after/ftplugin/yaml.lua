febd117560ed32d77b9fad8098cdcfa5491a14e5
Michael <michaeljohannpeter@gmail.com>
2022-01-18 20:43:44 +0100

    move yaml bindings to after/yaml

--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -2,3 +2,16 @@ local vo = vim.opt_local
 vo.tabstop = 2
 vo.shiftwidth = 2
 vo.softtabstop = 2
+
+local wk = require("which-key")
+default_options = {noremap = true, silent = true}
+wk.register({
+    y = {
+        name = "YAML",
+        s = {"<cmd>YAMLView<cr>", "Show Path"},
+        q = {"<cmd>YAMLQuickfix<cr>", "Generate quickfix list"},
+        t = {"<cmd>YAMLTelescope<cr>", "Open Telescope"},
+        k = {"<cmd>YAMLYankKey<cr>", "Yank key"},
+        v = {"<cmd>YAMLYankValue<cr>", "Yank value"}
+    }
+}, {prefix = "<leader>", mode = "n", default_options})
