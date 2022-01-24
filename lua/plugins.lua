ed178d239d5c5c7f3e4c1e261f63ddba4182980b
Michael <michaeljohannpeter@gmail.com>
2022-01-24 10:09:07 +0100

    Revert "use themer.lua for managing colorschemes"
    This reverts commit 475c9c5aeef801696c826db84f269f71a1c32a0c.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -245,7 +245,6 @@ use {"sidebar-nvim/sidebar.nvim", config = get_config("sidebar")}
 
 use {"ray-x/go.nvim", config = get_config("go")}
 
-use({"themercorp/themer.lua", config = get_config("themer")})
 -- TODO: ????
 -- https://github.com/glepnir/lspsaga.nvim
 -- use 'glepnir/lspsaga.nvim'
