a20e6b52f579bf5c746695bc97d8db1adcd714fc
Michael <michaeljohannpeter@gmail.com>
2022-04-13 16:51:30 +0200

    update and autoformat nvim-tree config

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,6 +1,5 @@
 local g = vim.g
 
-g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
 g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_root_folder_modifier = ":~" -- This is the default. See :help filename-modifiers for more options
@@ -10,140 +9,153 @@ g.nvim_tree_icon_padding = " " -- one space by default, used for rendering the s
 g.nvim_tree_symlink_arrow = " >> " --  defaults to ' ➛ '. used as a separator between symlinks' source and target.
 g.nvim_tree_respect_buf_cwd = 1 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 
-g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1, folder_arrows = 1}
+g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1 }
 g.nvim_tree_icons = {
-    default = "",
-    symlink = "",
-    git = {
-        unstaged = "",
-        staged = "S",
-        unmerged = "",
-        renamed = "➜",
-        deleted = "",
-        untracked = "U",
-        ignored = "◌"
-    },
-    folder = {
-        default = "",
-        open = "",
-        empty = "",
-        empty_open = "",
-        symlink = ""
-    }
+	default = "",
+	symlink = "",
+	git = {
+		unstaged = "",
+		staged = "S",
+		unmerged = "",
+		renamed = "➜",
+		deleted = "",
+		untracked = "U",
+		ignored = "◌",
+	},
+	folder = {
+		default = "",
+		open = "",
+		empty = "",
+		empty_open = "",
+		symlink = "",
+	},
 }
-local tree_cb = require"nvim-tree.config".nvim_tree_callback
-require("nvim-tree").setup {
-    -- disables netrw completely
-    disable_netrw = true,
-    -- hijack netrw window on startup
-    hijack_netrw = true,
-    -- open the tree when running this setup function
-    open_on_setup = false,
-    -- will not open on setup if the filetype is in this list
-    ignore_ft_on_setup = {},
-    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-    open_on_tab = true,
-    -- hijack the cursor in the tree to put it at the start of the filename
-    hijack_cursor = true,
-    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-    update_cwd = true,
-    -- this option hides files and folders starting with a dot `.`
-    hide_dotfiles = true,
-    -- show lsp diagnostics in the signcolumn
-    diagnostics = {
-        enabled = true,
-        icon = {hint = "", info = "", warning = "", error = ""}
-    },
-    git = {ignore = true},
-    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-    update_focused_file = {
-        -- enables the feature
-        enable = true,
-        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-        -- only relevant when `update_focused_file.enable` is true
-        update_cwd = true,
-        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-        ignore_list = {".git", "node_modules", ".cache"}
-    },
-    -- configuration options for the system open command (`s` in the tree by default)
-    system_open = {
-        -- the command to run this, leaving nil should work in most cases
-        cmd = nil,
-        -- the command arguments as a list
-        args = {}
-    },
+local tree_cb = require("nvim-tree.config").nvim_tree_callback
+require("nvim-tree").setup({
+	-- disables netrw completely
+	disable_netrw = true,
+	-- hijack netrw window on startup
+	hijack_netrw = true,
+	-- open the tree when running this setup function
+	open_on_setup = false,
+	-- will not open on setup if the filetype is in this list
+	ignore_ft_on_setup = {},
+	-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+	open_on_tab = true,
+	-- hijack the cursor in the tree to put it at the start of the filename
+	hijack_cursor = true,
+	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+	update_cwd = true,
+	-- this option hides files and folders starting with a dot `.`
+	hide_dotfiles = true,
+	-- show lsp diagnostics in the signcolumn
+	diagnostics = {
+		enabled = true,
+		icon = { hint = "", info = "", warning = "", error = "" },
+	},
+	git = { ignore = true },
+	-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+	update_focused_file = {
+		-- enables the feature
+		enable = true,
+		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+		-- only relevant when `update_focused_file.enable` is true
+		update_cwd = true,
+		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+		ignore_list = { ".git", "node_modules", ".cache" },
+	},
+	-- configuration options for the system open command (`s` in the tree by default)
+	system_open = {
+		-- the command to run this, leaving nil should work in most cases
+		cmd = nil,
+		-- the command arguments as a list
+		args = {},
+	},
 
-    trash = {cmd = "trash-put", require_confirm = true},
+	trash = { cmd = "trash-put", require_confirm = true },
 
-    actions = {
-        change_dir = {enable = true, global = false},
-        open_file = {
-            quit_on_open = false,
-            resize_window = false,
-            window_picker = {
-                enable = true,
-                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-                exclude = {
-                    filetype = {
-                        "notify", "packer", "qf", "diff", "fugitive",
-                        "fugitiveblame"
-                    },
-                    buftype = {"nofile", "terminal", "help"}
-                }
-            }
-        }
-    },
-
-    view = {
-        -- show line numbers in tree disabled
-        number = false,
-        relativenumber = false,
-        -- width of the window, can be either a number (columns) or a string in `%`
-        width = 30,
-        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-        side = "left",
-        -- if true the tree will resize itself after opening a file
-        auto_resize = false,
-        mappings = {
-            -- custom only false will merge the list with the default mappings
-            -- if true, it will only use your list to set the mappings
-            custom_only = true,
-            -- list of mappings to set on the tree manually
-            list = {
-                {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")},
-                {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
-                {key = "<C-v>", cb = tree_cb("vsplit")},
-                {key = "<C-x>", cb = tree_cb("split")},
-                {key = "<C-t>", cb = tree_cb("tabnew")},
-                {key = "<", cb = tree_cb("prev_sibling")},
-                {key = ">", cb = tree_cb("next_sibling")},
-                {key = "P", cb = tree_cb("parent_node")},
-                {key = "<BS>", cb = tree_cb("close_node")},
-                {key = "<S-CR>", cb = tree_cb("close_node")},
-                {key = "<Tab>", cb = tree_cb("preview")},
-                {key = "K", cb = tree_cb("first_sibling")},
-                {key = "J", cb = tree_cb("last_sibling")},
-                {key = "I", cb = tree_cb("toggle_ignored")},
-                {key = "H", cb = tree_cb("toggle_dotfiles")},
-                {key = "R", cb = tree_cb("refresh")},
-                {key = "a", cb = tree_cb("create")},
-                {key = "d", cb = tree_cb("remove")},
-                {key = "r", cb = tree_cb("rename")},
-                {key = "<C-r>", cb = tree_cb("full_rename")},
-                {key = "x", cb = tree_cb("cut")},
-                {key = "c", cb = tree_cb("copy")},
-                {key = "p", cb = tree_cb("paste")},
-                {key = "y", cb = tree_cb("copy_name")},
-                {key = "Y", cb = tree_cb("copy_path")},
-                {key = "gy", cb = tree_cb("copy_absolute_path")},
-                {key = "[c", cb = tree_cb("prev_git_item")},
-                {key = "]c", cb = tree_cb("next_git_item")},
-                {key = "-", cb = tree_cb("dir_up")},
-                {key = "s", cb = tree_cb("system_open")},
-                {key = "q", cb = tree_cb("close")},
-                {key = "g?", cb = tree_cb("toggle_help")}
-            }
-        }
-    }
-}
+	actions = {
+		change_dir = { enable = true, global = false },
+		open_file = {
+			quit_on_open = false,
+			resize_window = false,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = {
+						"notify",
+						"packer",
+						"qf",
+						"diff",
+						"fugitive",
+						"fugitiveblame",
+					},
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
+	},
+	renderer = {
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+	},
+	view = {
+		-- show line numbers in tree disabled
+		number = false,
+		relativenumber = false,
+		-- width of the window, can be either a number (columns) or a string in `%`
+		width = 30,
+		-- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+		side = "left",
+		-- if true the tree will resize itself after opening a file
+		auto_resize = false,
+		mappings = {
+			-- custom only false will merge the list with the default mappings
+			-- if true, it will only use your list to set the mappings
+			custom_only = true,
+			-- list of mappings to set on the tree manually
+			list = {
+				{ key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb("edit") },
+				{ key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
+				{ key = "<C-v>", cb = tree_cb("vsplit") },
+				{ key = "<C-x>", cb = tree_cb("split") },
+				{ key = "<C-t>", cb = tree_cb("tabnew") },
+				{ key = "<", cb = tree_cb("prev_sibling") },
+				{ key = ">", cb = tree_cb("next_sibling") },
+				{ key = "P", cb = tree_cb("parent_node") },
+				{ key = "<BS>", cb = tree_cb("close_node") },
+				{ key = "<S-CR>", cb = tree_cb("close_node") },
+				{ key = "<Tab>", cb = tree_cb("preview") },
+				{ key = "K", cb = tree_cb("first_sibling") },
+				{ key = "J", cb = tree_cb("last_sibling") },
+				{ key = "I", cb = tree_cb("toggle_ignored") },
+				{ key = "H", cb = tree_cb("toggle_dotfiles") },
+				{ key = "R", cb = tree_cb("refresh") },
+				{ key = "a", cb = tree_cb("create") },
+				{ key = "d", cb = tree_cb("remove") },
+				{ key = "r", cb = tree_cb("rename") },
+				{ key = "<C-r>", cb = tree_cb("full_rename") },
+				{ key = "x", cb = tree_cb("cut") },
+				{ key = "c", cb = tree_cb("copy") },
+				{ key = "p", cb = tree_cb("paste") },
+				{ key = "y", cb = tree_cb("copy_name") },
+				{ key = "Y", cb = tree_cb("copy_path") },
+				{ key = "gy", cb = tree_cb("copy_absolute_path") },
+				{ key = "[c", cb = tree_cb("prev_git_item") },
+				{ key = "]c", cb = tree_cb("next_git_item") },
+				{ key = "-", cb = tree_cb("dir_up") },
+				{ key = "s", cb = tree_cb("system_open") },
+				{ key = "q", cb = tree_cb("close") },
+				{ key = "g?", cb = tree_cb("toggle_help") },
+			},
+		},
+	},
+})
