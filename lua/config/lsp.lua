be419c96e5e13c679040f5909c70c0f8d76e402a
Michael <michaeljohannpeter@gmail.com>
2022-04-16 01:11:01 +0200

    switch from jedi to pyright

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,12 +1,32 @@
 local nvim_lsp = require("lspconfig")
 
+-- detect python venv
+-- https://github.com/neovim/nvim-lspconfig/issues/500#issuecomment-851247107
+local util = require("lspconfig/util")
+local path = util.path
+local function get_python_path(workspace)
+	-- Use activated virtualenv.
+	if vim.env.VIRTUAL_ENV then
+		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+	end
+	-- Find and use virtualenv in workspace directory.
+	for _, pattern in ipairs({ "*", ".*" }) do
+		local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+		if match ~= "" then
+			return path.join(path.dirname(match), "bin", "python")
+		end
+	end
+	-- Fallback to system Python.
+	return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+end
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 local servers = {
 	"gopls",
 	"bashls",
-	"jedi_language_server",
+	"pyright",
 	"dockerls",
 	"terraformls",
 	"sumneko_lua",
@@ -22,6 +42,11 @@ for _, lsp in ipairs(servers) do
 			-- disable formatting for LSP clients as this is handled by null-ls
 			client.resolved_capabilities.document_formatting = false
 			client.resolved_capabilities.document_range_formatting = false
+		end,
+		before_init = function(_, config)
+			if lsp == "pyright" then
+				config.settings.python.pythonPath = get_python_path(config.root_dir)
+			end
 		end,
 		capabilities = capabilities,
 		settings = {
--- lua/config/project.lua
+++ lua/config/project.lua
@@ -1,7 +1,13 @@
-require("project_nvim").setup {
-    patterns = {
-        ".git", "package.json", ".terraform", "go.mod", "requirements.yml"
-    },
-    -- detection_methods = { "lsp", "pattern" },
-    detection_methods = {"pattern"}
-}
+require("project_nvim").setup({
+	patterns = {
+		".git",
+		"package.json",
+		".terraform",
+		"go.mod",
+		"requirements.yml",
+		"pyrightconfig.json",
+		"pyproject.toml",
+	},
+	-- detection_methods = { "lsp", "pattern" },
+	detection_methods = { "pattern" },
+})
