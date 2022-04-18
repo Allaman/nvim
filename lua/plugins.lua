93760c16b3fe5ab4756d52dfb1e1bc747f4a55cf
Michael <michaeljohannpeter@gmail.com>
2022-04-18 19:36:14 +0200

    clean up plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,227 +22,228 @@ end
 
 -- initialize and configure packer
 local packer = require("packer")
+
 packer.init({
 	enable = true, -- enable profiling via :PackerCompile profile=true
 	threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
 })
-local use = packer.use
-packer.reset()
 
--- actual plugins list
-use("wbthomason/packer.nvim")
+packer.startup(function(use)
+	-- actual plugins list
+	use("wbthomason/packer.nvim")
 
-use({
-	"nvim-telescope/telescope.nvim",
-	requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
-	config = get_config("telescope"),
-})
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+		config = get_config("telescope"),
+	})
 
-use({ "jvgrootveld/telescope-zoxide" })
-use({ "crispgm/telescope-heading.nvim" })
-use({ "nvim-telescope/telescope-symbols.nvim" })
-use({ "nvim-telescope/telescope-file-browser.nvim" })
-use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "jvgrootveld/telescope-zoxide" })
+	use({ "crispgm/telescope-heading.nvim" })
+	use({ "nvim-telescope/telescope-symbols.nvim" })
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
-use({ "kyazdani42/nvim-tree.lua", config = get_config("nvim-tree") })
+	use({ "kyazdani42/nvim-tree.lua", config = get_config("nvim-tree") })
 
-use({ "numToStr/Navigator.nvim", config = get_config("navigator") })
+	use({ "numToStr/Navigator.nvim", config = get_config("navigator") })
 
-use({
-	"nvim-lualine/lualine.nvim",
-	config = get_config("lualine"),
-	event = "VimEnter",
-	requires = { "kyazdani42/nvim-web-devicons", opt = true },
-})
+	use({
+		"nvim-lualine/lualine.nvim",
+		config = get_config("lualine"),
+		event = "VimEnter",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-use({
-	"norcalli/nvim-colorizer.lua",
-	event = "BufReadPre",
-	config = get_config("colorizer"),
-})
+	use({
+		"norcalli/nvim-colorizer.lua",
+		event = "BufReadPre",
+		config = get_config("colorizer"),
+	})
 
-use({ "windwp/nvim-autopairs", config = get_config("autopairs") })
+	use({ "windwp/nvim-autopairs", config = get_config("autopairs") })
 
-use({
-	"nvim-treesitter/nvim-treesitter",
-	config = get_config("treesitter"),
-	run = ":TSUpdate",
-})
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		config = get_config("treesitter"),
+		run = ":TSUpdate",
+	})
 
-use("nvim-treesitter/nvim-treesitter-textobjects")
+	use("nvim-treesitter/nvim-treesitter-textobjects")
 
-use({
-	"hrsh7th/nvim-cmp",
-	requires = {
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{ "hrsh7th/cmp-cmdline" },
-		{ "f3fora/cmp-spell", { "hrsh7th/cmp-calc" }, { "lukas-reineke/cmp-rg" } },
-	},
-	config = get_config("cmp"),
-})
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-cmdline" },
+			{ "f3fora/cmp-spell", { "hrsh7th/cmp-calc" }, { "lukas-reineke/cmp-rg" } },
+		},
+		config = get_config("cmp"),
+	})
 
-use({ "rafamadriz/friendly-snippets" })
-use({
-	"L3MON4D3/LuaSnip",
-	requires = "saadparwaiz1/cmp_luasnip",
-	config = get_config("luasnip"),
-})
+	use({ "rafamadriz/friendly-snippets" })
+	use({
+		"L3MON4D3/LuaSnip",
+		requires = "saadparwaiz1/cmp_luasnip",
+		config = get_config("luasnip"),
+	})
 
--- requirement for Neogit
-use({
-	"sindrets/diffview.nvim",
-	cmd = {
-		"DiffviewOpen",
-		"DiffviewClose",
-		"DiffviewToggleFiles",
-		"DiffviewFocusFiles",
-	},
-	config = get_config("diffview"),
-})
+	-- requirement for Neogit
+	use({
+		"sindrets/diffview.nvim",
+		cmd = {
+			"DiffviewOpen",
+			"DiffviewClose",
+			"DiffviewToggleFiles",
+			"DiffviewFocusFiles",
+		},
+		config = get_config("diffview"),
+	})
 
-use({
-	"TimUntersberger/neogit",
-	requires = { "nvim-lua/plenary.nvim" },
-	cmd = "Neogit",
-	config = get_config("neogit"),
-})
+	use({
+		"TimUntersberger/neogit",
+		requires = { "nvim-lua/plenary.nvim" },
+		cmd = "Neogit",
+		config = get_config("neogit"),
+	})
 
-use({ "f-person/git-blame.nvim", config = get_config("git-blame") })
+	use({ "f-person/git-blame.nvim", config = get_config("git-blame") })
 
-use({
-	"lewis6991/gitsigns.nvim",
-	requires = { "nvim-lua/plenary.nvim" },
-	event = "BufReadPre",
-	config = get_config("gitsigns"),
-})
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		event = "BufReadPre",
+		config = get_config("gitsigns"),
+	})
 
-use("p00f/nvim-ts-rainbow")
+	use("p00f/nvim-ts-rainbow")
 
-use({
-	"kevinhwang91/nvim-bqf",
-	requires = { { "junegunn/fzf", module = "nvim-bqf" } },
-})
+	use({
+		"kevinhwang91/nvim-bqf",
+		requires = { { "junegunn/fzf", module = "nvim-bqf" } },
+	})
 
-use({
-	"akinsho/nvim-bufferline.lua",
-	requires = "kyazdani42/nvim-web-devicons",
-	event = "BufReadPre",
-	config = get_config("bufferline"),
-})
+	use({
+		"akinsho/nvim-bufferline.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+		event = "BufReadPre",
+		config = get_config("bufferline"),
+	})
 
-use("famiu/bufdelete.nvim")
+	use("famiu/bufdelete.nvim")
 
-use({ "neovim/nvim-lspconfig", config = get_config("lsp") })
+	use({ "neovim/nvim-lspconfig", config = get_config("lsp") })
 
-use({
-	"ray-x/lsp_signature.nvim",
-	require = { "neovim/nvim-lspconfig" },
-	config = get_config("lsp-signature"),
-})
+	use({
+		"ray-x/lsp_signature.nvim",
+		require = { "neovim/nvim-lspconfig" },
+		config = get_config("lsp-signature"),
+	})
 
-use({ "onsails/lspkind-nvim", requires = { "famiu/bufdelete.nvim" } })
+	use({ "onsails/lspkind-nvim", requires = { "famiu/bufdelete.nvim" } })
 
-use({
-	"jose-elias-alvarez/null-ls.nvim",
-	requires = { { "nvim-lua/plenary.nvim" } },
-	config = get_config("null-ls"),
-})
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+		config = get_config("null-ls"),
+	})
 
-use({
-	"simrat39/symbols-outline.nvim",
-	cmd = { "SymbolsOutline" },
-	config = get_config("symbols"),
-})
+	use({
+		"simrat39/symbols-outline.nvim",
+		cmd = { "SymbolsOutline" },
+		config = get_config("symbols"),
+	})
 
-use({
-	"lukas-reineke/indent-blankline.nvim",
-	event = "BufReadPre",
-	config = [[require("config/indent-blankline")]],
-})
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufReadPre",
+		config = [[require("config/indent-blankline")]],
+	})
 
-use({
-	"akinsho/nvim-toggleterm.lua",
-	keys = { "<C-n>", "<leader>fl", "<leader>gt" },
-	config = get_config("toggleterm"),
-})
+	use({
+		"akinsho/nvim-toggleterm.lua",
+		keys = { "<C-n>", "<leader>fl", "<leader>gt" },
+		config = get_config("toggleterm"),
+	})
 
-use({
-	"folke/trouble.nvim",
-	requires = "kyazdani42/nvim-web-devicons",
-	cmd = { "TroubleToggle", "Trouble" },
-	config = get_config("trouble"),
-})
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		cmd = { "TroubleToggle", "Trouble" },
+		config = get_config("trouble"),
+	})
 
-use({
-	"folke/todo-comments.nvim",
-	requires = "nvim-lua/plenary.nvim",
-	cmd = { "TodoTrouble", "TodoTelescope" },
-	event = "BufReadPost",
-	config = get_config("todo"),
-})
+	use({
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		event = "BufReadPost",
+		config = get_config("todo"),
+	})
 
-use({ "ahmedkhalf/project.nvim", config = get_config("project") })
+	use({ "ahmedkhalf/project.nvim", config = get_config("project") })
 
-use("ironhouzi/starlite-nvim")
+	use("ironhouzi/starlite-nvim")
 
-use({ "folke/which-key.nvim", config = get_config("which") })
+	use({ "folke/which-key.nvim", config = get_config("which") })
 
-use("junegunn/vim-easy-align") -- no lua alternative
+	use("junegunn/vim-easy-align") -- no lua alternative
 
-use({ "rhysd/vim-grammarous", cmd = "GrammarousCheck" })
+	use({ "rhysd/vim-grammarous", cmd = "GrammarousCheck" })
 
-use({ "RRethy/vim-illuminate", event = "CursorHold" })
+	use({ "RRethy/vim-illuminate", event = "CursorHold" })
 
-use({
-	"ptzz/lf.vim",
-	requires = "voldikss/vim-floaterm",
-	config = get_config("lf"),
-})
+	use({
+		"ptzz/lf.vim",
+		requires = "voldikss/vim-floaterm",
+		config = get_config("lf"),
+	})
 
-use({ "EdenEast/nightfox.nvim", config = get_config("nightfox") })
+	use({ "EdenEast/nightfox.nvim", config = get_config("nightfox") })
 
-use({
-	"karb94/neoscroll.nvim",
-	keys = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-e>", "zt", "zz", "zb" },
-	config = get_config("neoscroll"),
-})
+	use({
+		"karb94/neoscroll.nvim",
+		keys = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-e>", "zt", "zz", "zb" },
+		config = get_config("neoscroll"),
+	})
 
-use({
-	"ThePrimeagen/harpoon",
-	requires = { "nvim-lua/plenary.nvim" },
-	config = get_config("harpoon"),
-})
+	use({
+		"ThePrimeagen/harpoon",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = get_config("harpoon"),
+	})
 
-use({ "folke/zen-mode.nvim", cmd = "ZenMode", config = get_config("zen-mode") })
+	use({ "folke/zen-mode.nvim", cmd = "ZenMode", config = get_config("zen-mode") })
 
-use({ "folke/twilight.nvim", config = get_config("twilight") })
+	use({ "folke/twilight.nvim", config = get_config("twilight") })
 
-use({ "tweekmonster/startuptime.vim" })
+	use({ "tweekmonster/startuptime.vim" })
 
-use({ "ggandor/lightspeed.nvim", event = "BufReadPre" })
+	use({ "ggandor/lightspeed.nvim", event = "BufReadPre" })
 
-use({ "cuducos/yaml.nvim", ft = { "yaml" } })
+	use({ "cuducos/yaml.nvim", ft = { "yaml" } })
 
-use({ "ray-x/go.nvim", config = get_config("go") })
+	use({ "ray-x/go.nvim", config = get_config("go") })
 
-use({ "LudoPinelli/comment-box.nvim", config = get_config("comment-box") })
+	use({ "LudoPinelli/comment-box.nvim", config = get_config("comment-box") })
 
-use({ "rcarriga/nvim-notify", config = get_config("notify") })
+	use({ "rcarriga/nvim-notify", config = get_config("notify") })
 
-use({ "echasnovski/mini.nvim", branch = "stable", config = get_config("mini") })
+	use({ "echasnovski/mini.nvim", branch = "stable", config = get_config("mini") })
 
-use({
-	"https://gitlab.com/yorickpeterse/nvim-window.git",
-	config = get_config("nvim-window"),
-})
+	use({
+		"https://gitlab.com/yorickpeterse/nvim-window.git",
+		config = get_config("nvim-window"),
+	})
 
-use({ "waylonwalker/Telegraph.nvim", config = require("telegraph").setup({}) })
+	use({ "waylonwalker/Telegraph.nvim", config = require("telegraph").setup({}) })
 
-use({ "rhysd/conflict-marker.vim" })
+	use({ "rhysd/conflict-marker.vim" })
 
-use({ "edluffy/specs.nvim", config = get_config("specs") })
+	use({ "edluffy/specs.nvim", config = get_config("specs") })
+end)
 
 -- TODO: ????
 -- use {"lukas-reineke/headlines.nvim", config = get_config("headlines")}
