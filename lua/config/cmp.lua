13451119ced8927819f90cbdeee50686dd7abed0
Michael <michaeljohannpeter@gmail.com>
2022-04-18 19:35:15 +0200

    improve lsp-kind settings

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -2,48 +2,18 @@
 local cmp = require("cmp")
 local lspkind = require("lspkind")
 
-lspkind.init({
-	symbol_map = {
-		Text = "",
-		Method = "",
-		Function = "",
-		Constructor = "",
-		Field = "ﰠ",
-		Variable = "",
-		Class = "ﴯ",
-		Interface = "",
-		Module = "",
-		Property = "ﰠ",
-		Unit = "塞",
-		Value = "",
-		Enum = "",
-		Keyword = "",
-		Snippet = "",
-		Color = "",
-		File = "",
-		Reference = "",
-		Folder = "",
-		EnumMember = "",
-		Constant = "",
-		Struct = "פּ",
-		Event = "",
-		Operator = "",
-		TypeParameter = "",
-	},
-})
-
 cmp.setup({
 	formatting = {
 		format = lspkind.cmp_format({
 			with_text = false,
 			maxwidth = 50,
+			mode = "symbol",
 			menu = {
 				buffer = "BUF",
 				rg = "RG",
 				nvim_lsp = "LSP",
 				path = "PATH",
 				luasnip = "SNIP",
-				-- vsnip = "SNIP",
 				calc = "CALC",
 				spell = "SPELL",
 				emoji = "EMOJI",
@@ -53,14 +23,14 @@ cmp.setup({
 	experimental = { native_menu = false, ghost_text = false },
 	snippet = {
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+			require("luasnip").lsp_expand(args.body)
 		end,
 	},
 	mapping = {
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-u>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
 		["<CR>"] = cmp.mapping.confirm({
@@ -71,7 +41,7 @@ cmp.setup({
 			if cmp.visible() then
 				cmp.select_next_item()
 			else
-				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+				fallback()
 			end
 		end, { "i", "s" }),
 		["<S-Tab>"] = cmp.mapping(function()
