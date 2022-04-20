bf386a207e57bcc8fc16de16e809f26255f7cc62
Michael <michaeljohannpeter@gmail.com>
2022-04-21 01:45:44 +0200

    which-key appearance

--- lua/config/which.lua
+++ lua/config/which.lua
@@ -37,7 +37,7 @@ require("which-key").setup({
 		group = "+", -- symbol prepended to a group
 	},
 	window = {
-		border = "none", -- none, single, double, shadow
+		border = "single", -- none, single, double, shadow
 		position = "bottom", -- bottom, top
 		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
 		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
@@ -45,8 +45,8 @@ require("which-key").setup({
 	layout = {
 		height = { min = 4, max = 25 }, -- min and max height of the columns
 		width = { min = 20, max = 50 }, -- min and max width of the columns
-		spacing = 3, -- spacing between columns
-		align = "left", -- align columns left, center or right
+		spacing = 2, -- spacing between columns
+		align = "center", -- align columns left, center or right
 	},
 	ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
 	hidden = {
