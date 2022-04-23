e9322d73641d7b0cdcf7fefc6b7a3e5c001c5d2d
Michael <michaeljohannpeter@gmail.com>
2022-04-23 13:30:46 +0200

    add autocommand for enabling spelling

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,6 +1,5 @@
-vim.opt_local.spell = true
 vim.opt_local.wrap = true
 
 -- match and highlight hyperlinks
 vim.fn.matchadd("matchURL", [[http[s]\?:\/\/[[:alnum:]%\/_#.-]*]])
-vim.cmd "hi matchURL guifg=DodgerBlue"
+vim.cmd("hi matchURL guifg=DodgerBlue")
--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -45,3 +45,9 @@ api.nvim_create_autocmd(
 	{ "InsertEnter", "WinLeave" },
 	{ pattern = "*", command = "set nocursorline", group = cursorGrp }
 )
+
+-- Enable spell checking for certain file types
+vim.api.nvim_create_autocmd(
+	{ "BufRead", "BufNewFile" },
+	{ pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" }
+)
