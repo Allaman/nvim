75963d12f0b3ec0f30fbafffeb24ec648110461f
Michael <michaeljohannpeter@gmail.com>
2022-05-12 00:53:15 +0200

    remove conflicting keybindings from cmp

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -26,8 +26,6 @@ cmp.setup({
 		end,
 	},
 	mapping = {
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-u>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
