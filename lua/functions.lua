4d04259d649df76458192abbbcad52bb87ca63da
Michael <michaeljohannpeter@gmail.com>
2022-05-30 21:17:30 +0200

    bufferline replace close_command with custom function

--- lua/functions.lua
+++ lua/functions.lua
@@ -59,4 +59,49 @@ M.toggle_diagnostics = function()
 	end
 end
 
+--@author kikito
+---@see https://codereview.stackexchange.com/questions/268130/get-list-of-buffers-from-current-neovim-instance
+function M.get_listed_buffers()
+	local buffers = {}
+	local len = 0
+	for buffer = 1, vim.fn.bufnr("$") do
+		if vim.fn.buflisted(buffer) == 1 then
+			len = len + 1
+			buffers[len] = buffer
+		end
+	end
+
+	return buffers
+end
+
+function M.bufdelete(bufnum)
+	require("bufdelete").bufdelete(bufnum, true)
+end
+
+-- when there is no buffer left show Alpha dashboard
+vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+	pattern = "BDeletePre",
+	group = "alpha_on_empty",
+	callback = function(event)
+		local found_non_empty_buffer = false
+		local buffers = M.get_listed_buffers()
+
+		for _, bufnr in ipairs(buffers) do
+			if not found_non_empty_buffer then
+				local name = vim.api.nvim_buf_get_name(bufnr)
+				local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+
+				if bufnr ~= event.buf and name ~= "" and ft ~= "Alpha" then
+					found_non_empty_buffer = true
+				end
+			end
+		end
+
+		if not found_non_empty_buffer then
+			vim.cmd([[:Alpha]])
+		end
+	end,
+})
+
 return M
