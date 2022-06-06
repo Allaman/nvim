a36f0956fef454ce497eec1411224a76db4a684e
Michael <michaeljohannpeter@gmail.com>
2022-06-06 11:38:43 +0200

    clean up obsolet lua lsp settings

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -93,20 +93,15 @@ for _, lsp in ipairs(servers) do
 				filetypes = { "lua" },
 				runtime = {
 					version = "LuaJIT",
-					path = vim.split(package.path, ";"),
 				},
 				completion = { enable = true, callSnippet = "Both" },
 				diagnostics = {
 					enable = true,
 					globals = { "vim", "describe" },
-					disable = { "lowercase-global" },
 				},
 				workspace = {
 					library = {
 						vim.api.nvim_get_runtime_file("", true),
-						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-						[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-						[vim.fn.expand("/usr/share/awesome/lib")] = true,
 					},
 					-- adjust these two values if your performance is not optimal
 					maxPreload = 2000,
