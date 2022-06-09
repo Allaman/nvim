2ca848b4eefc0a6b2289629b747275ca1d6c8d88
Michael <michaeljohannpeter@gmail.com>
2022-06-09 22:15:05 +0200

    comment out not need bufferline filters

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -31,24 +31,24 @@ require("bufferline").setup({
 				return false
 			end
 			-- filter out by buffer name
-			if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-				return true
-			else
-				return false
-			end
+			-- if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+			-- 	return true
+			-- else
+			-- 	return false
+			-- end
 			-- filter out based on arbitrary rules
 			-- e.g. filter out vim wiki buffer from tabline in your work repo
-			if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-				return true
-			else
-				return false
-			end
+			-- if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+			-- 	return true
+			-- else
+			-- 	return false
+			-- end
 			-- filter out by it's index number in list (don't show first buffer)
-			if buf_numbers[1] ~= buf_number then
-				return true
-			else
-				return false
-			end
+			-- if buf_numbers[1] ~= buf_number then
+			-- 	return true
+			-- else
+			-- 	return false
+			-- end
 		end,
 		offsets = {
 			{
