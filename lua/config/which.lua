6bb29c97d3e5aa67e0b16cf8d06614a27e353aa7
Michael <michaeljohannpeter@gmail.com>
2022-06-12 12:48:53 +0200

    move LSP mappings from which.lua to lsp.lua

--- lua/config/which.lua
+++ lua/config/which.lua
@@ -189,45 +189,6 @@ wk.register({
 			"Open File 4",
 		},
 	},
-	l = {
-		name = "LSP",
-		A = {
-			"<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>",
-			"Add Workspace Folder",
-		},
-		D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Go To Declaration" },
-		I = {
-			"<cmd>lua vim.lsp.buf.implementation()<cr>",
-			"Show implementations",
-		},
-		L = {
-			"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>",
-			"List Workspace Folders",
-		},
-		R = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-		S = {
-			"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
-			"Workspace Symbols",
-		},
-		W = {
-			"<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>",
-			"Remove Workspace Folder",
-		},
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-		d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Go To Definition" },
-		e = { "<cmd>Telescope diagnostics bufnr=0<cr>", "Document Diagnostics" },
-		f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
-		i = { "<cmd>LspInfo<cr>", "Connected Language Servers" },
-		k = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover Commands" },
-		l = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Line diagnostics" },
-		n = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
-		p = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
-		q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-		r = { "<cmd>lua vim.lsp.buf.references()<cr>", "References" },
-		s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-		t = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type Definition" },
-		w = { "<cmd>Telescope diagnostics<cr>", "Workspace Diagnostics" },
-	},
 	m = {
 		name = "Misc",
 		a = {
