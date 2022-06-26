9f3ed73de50416aa6edb6c8f3672728094598f79
Michael <michaeljohannpeter@gmail.com>
2022-06-26 12:17:29 +0200

    add folding capabilites to lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -22,6 +22,10 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
+}
 
 local servers = {
 	"bashls",
