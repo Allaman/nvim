2177875c97cde09454b411846cb8b6dc6ae57526
Michael <michaeljohannpeter@gmail.com>
2022-07-04 00:37:16 +0200

    allow to configure list and listchars

--- lua/user-conf.lua
+++ lua/user-conf.lua
@@ -10,6 +10,11 @@ M.grepprg = "rg --hidden --vimgrep --smart-case --"
 M.number = true
 -- set relative numbered lines
 M.relative_number = true
+-- enable or disable listchars
+M.list = true
+-- which list chars to schow
+-- M.listchars = "eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣"
+M.listchars = "eol:¬,tab:>·,trail:~,extends:>,precedes:<"
 -- enable PackerSync on plugins.lua save
 M.packer_auto_sync = false
 
