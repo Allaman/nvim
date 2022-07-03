814a2883dc84fe2ae2e7730c2062fe4d729b3a35
Michael <michaeljohannpeter@gmail.com>
2022-07-03 23:52:50 +0200

    autoformatting

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -1,35 +1,33 @@
 local nls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 nls.setup({
-  sources = {
-    nls.builtins.formatting.stylua,
-    nls.builtins.diagnostics.eslint,
-    nls.builtins.formatting.prettier.with({
-      extra_args = { "--single-quote", "false" },
-    }),
-    nls.builtins.formatting.terraform_fmt,
-    nls.builtins.formatting.black,
-    nls.builtins.formatting.goimports,
-    nls.builtins.formatting.gofumpt,
-    nls.builtins.formatting.latexindent.with({
-      extra_args = { "-g", "/dev/null" }, -- https://github.com/cmhughes/latexindent.pl/releases/tag/V3.9.3
-    }),
-    nls.builtins.code_actions.shellcheck,
-    nls.builtins.diagnostics.vale,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-    require("functions").custom_lsp_attach(client, bufnr)
-  end,
+	sources = {
+		nls.builtins.formatting.stylua,
+		nls.builtins.diagnostics.eslint,
+		nls.builtins.formatting.prettier.with({
+			extra_args = { "--single-quote", "false" },
+		}),
+		nls.builtins.formatting.terraform_fmt,
+		nls.builtins.formatting.black,
+		nls.builtins.formatting.goimports,
+		nls.builtins.formatting.gofumpt,
+		nls.builtins.formatting.latexindent.with({
+			extra_args = { "-g", "/dev/null" }, -- https://github.com/cmhughes/latexindent.pl/releases/tag/V3.9.3
+		}),
+		nls.builtins.code_actions.shellcheck,
+		nls.builtins.diagnostics.vale,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+		require("functions").custom_lsp_attach(client, bufnr)
+	end,
 })
-
--- vim.diagnostic.setloclist
