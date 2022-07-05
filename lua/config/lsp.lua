d56b66b11b0290e789cf181f8d74fc0646042a30
Michael <michaeljohannpeter@gmail.com>
2022-07-06 00:08:09 +0200

    replace tabs with spaces

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -5,158 +5,158 @@ local nvim_lsp = require("lspconfig")
 local util = require("lspconfig/util")
 local path = util.path
 local function get_python_path(workspace)
-	-- Use activated virtualenv.
-	if vim.env.VIRTUAL_ENV then
-		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
-	end
-	-- Find and use virtualenv in workspace directory.
-	for _, pattern in ipairs({ "*", ".*" }) do
-		local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
-		if match ~= "" then
-			return path.join(path.dirname(match), "bin", "python")
-		end
-	end
-	-- Fallback to system Python.
-	return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+  end
+  -- Find and use virtualenv in workspace directory.
+  for _, pattern in ipairs({ "*", ".*" }) do
+    local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+    if match ~= "" then
+      return path.join(path.dirname(match), "bin", "python")
+    end
+  end
+  -- Fallback to system Python.
+  return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
 }
 
 local servers = {
-	"bashls",
-	"dockerls",
-	"jsonls",
-	"pyright",
-	"sumneko_lua",
-	"terraformls",
-	"texlab",
-	"tsserver",
-	"yamlls",
+  "bashls",
+  "dockerls",
+  "jsonls",
+  "pyright",
+  "sumneko_lua",
+  "terraformls",
+  "texlab",
+  "tsserver",
+  "yamlls",
 }
 -- Use a loop to conveniently call 'setup' on multiple servers
 for _, lsp in ipairs(servers) do
-	nvim_lsp[lsp].setup({
-		on_attach = function(client, bufnr)
-			require("functions").custom_lsp_attach(client, bufnr)
-		end,
-		before_init = function(_, config)
-			if lsp == "pyright" then
-				config.settings.python.pythonPath = get_python_path(config.root_dir)
-			end
-		end,
-		capabilities = capabilities,
-		settings = {
-			json = {
-				format = { enabled = false },
-				schemas = {
-					{
-						description = "ESLint config",
-						fileMatch = { ".eslintrc.json", ".eslintrc" },
-						url = "http://json.schemastore.org/eslintrc",
-					},
-					{
-						description = "Package config",
-						fileMatch = { "package.json" },
-						url = "https://json.schemastore.org/package",
-					},
-					{
-						description = "Packer config",
-						fileMatch = { "packer.json" },
-						url = "https://json.schemastore.org/packer",
-					},
-					{
-						description = "Renovate config",
-						fileMatch = {
-							"renovate.json",
-							"renovate.json5",
-							".github/renovate.json",
-							".github/renovate.json5",
-							".renovaterc",
-							".renovaterc.json",
-						},
-						url = "https://docs.renovatebot.com/renovate-schema",
-					},
-					{
-						description = "OpenApi config",
-						fileMatch = { "*api*.json" },
-						url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json",
-					},
-				},
-			},
-			Lua = {
-				cmd = { "lua-language-server" },
-				filetypes = { "lua" },
-				runtime = {
-					version = "LuaJIT",
-				},
-				completion = { enable = true, callSnippet = "Both" },
-				diagnostics = {
-					enable = true,
-					globals = { "vim", "describe" },
-				},
-				workspace = {
-					library = {
-						vim.api.nvim_get_runtime_file("", true),
-					},
-					-- adjust these two values if your performance is not optimal
-					maxPreload = 2000,
-					preloadFileSize = 1000,
-				},
-				telemetry = { enable = false },
-			},
-			redhat = { telemetry = { enabled = false } },
-			texlab = {
-				auxDirectory = ".",
-				bibtexFormatter = "texlab",
-				build = {
-					args = {
-						"--keep-intermediates",
-						"--keep-logs",
-						"--synctex",
-						"%f",
-					},
-					executable = "tectonic",
-					forwardSearchAfter = false,
-					onSave = false,
-				},
-				chktex = { onEdit = false, onOpenAndSave = false },
-				diagnosticsDelay = 300,
-				formatterLineLength = 80,
-				forwardSearch = { args = {} },
-				latexFormatter = "latexindent",
-				latexindent = { modifyLineBreaks = false },
-			},
-			yaml = {
-				schemaStore = {
-					enable = true,
-					url = "https://www.schemastore.org/api/json/catalog.json",
-				},
-				schemas = {
-					kubernetes = "*.yaml",
-					["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-					["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-					["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-					["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-					["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-					["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-					["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-					["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-					["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-					["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-					["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-					["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-				},
-				format = { enabled = false },
-				validate = false, -- TODO: conflicts between Kubernetes resources and kustomization.yaml
-				completion = true,
-				hover = true,
-			},
-		},
-		flags = { debounce_text_changes = 150 },
-	})
+  nvim_lsp[lsp].setup({
+    on_attach = function(client, bufnr)
+      require("functions").custom_lsp_attach(client, bufnr)
+    end,
+    before_init = function(_, config)
+      if lsp == "pyright" then
+        config.settings.python.pythonPath = get_python_path(config.root_dir)
+      end
+    end,
+    capabilities = capabilities,
+    settings = {
+      json = {
+        format = { enabled = false },
+        schemas = {
+          {
+            description = "ESLint config",
+            fileMatch = { ".eslintrc.json", ".eslintrc" },
+            url = "http://json.schemastore.org/eslintrc",
+          },
+          {
+            description = "Package config",
+            fileMatch = { "package.json" },
+            url = "https://json.schemastore.org/package",
+          },
+          {
+            description = "Packer config",
+            fileMatch = { "packer.json" },
+            url = "https://json.schemastore.org/packer",
+          },
+          {
+            description = "Renovate config",
+            fileMatch = {
+              "renovate.json",
+              "renovate.json5",
+              ".github/renovate.json",
+              ".github/renovate.json5",
+              ".renovaterc",
+              ".renovaterc.json",
+            },
+            url = "https://docs.renovatebot.com/renovate-schema",
+          },
+          {
+            description = "OpenApi config",
+            fileMatch = { "*api*.json" },
+            url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json",
+          },
+        },
+      },
+      Lua = {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        runtime = {
+          version = "LuaJIT",
+        },
+        completion = { enable = true, callSnippet = "Both" },
+        diagnostics = {
+          enable = true,
+          globals = { "vim", "describe" },
+        },
+        workspace = {
+          library = {
+            vim.api.nvim_get_runtime_file("", true),
+          },
+          -- adjust these two values if your performance is not optimal
+          maxPreload = 2000,
+          preloadFileSize = 1000,
+        },
+        telemetry = { enable = false },
+      },
+      redhat = { telemetry = { enabled = false } },
+      texlab = {
+        auxDirectory = ".",
+        bibtexFormatter = "texlab",
+        build = {
+          args = {
+            "--keep-intermediates",
+            "--keep-logs",
+            "--synctex",
+            "%f",
+          },
+          executable = "tectonic",
+          forwardSearchAfter = false,
+          onSave = false,
+        },
+        chktex = { onEdit = false, onOpenAndSave = false },
+        diagnosticsDelay = 300,
+        formatterLineLength = 80,
+        forwardSearch = { args = {} },
+        latexFormatter = "latexindent",
+        latexindent = { modifyLineBreaks = false },
+      },
+      yaml = {
+        schemaStore = {
+          enable = true,
+          url = "https://www.schemastore.org/api/json/catalog.json",
+        },
+        schemas = {
+          kubernetes = "*.yaml",
+          ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+          ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+          ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+          ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+          ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+          ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+          ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+          ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+          ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+        },
+        format = { enabled = false },
+        validate = false, -- TODO: conflicts between Kubernetes resources and kustomization.yaml
+        completion = true,
+        hover = true,
+      },
+    },
+    flags = { debounce_text_changes = 150 },
+  })
 end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,283 +6,283 @@ local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 -- returns the require for use in `config` parameter of packer's use
 -- expects the name of the config file
 local function get_config(name)
-	return string.format('require("config/%s")', name)
+  return string.format('require("config/%s")', name)
 end
 
 -- bootstrap packer if not installed
 if fn.empty(fn.glob(install_path)) > 0 then
-	fn.system({
-		"git",
-		"clone",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer...")
-	vim.api.nvim_command("packadd packer.nvim")
+  fn.system({
+    "git",
+    "clone",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer...")
+  vim.api.nvim_command("packadd packer.nvim")
 end
 
 -- initialize and configure packer
 local packer = require("packer")
 
 packer.init({
-	enable = true, -- enable profiling via :PackerCompile profile=true
-	threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
-	max_jobs = 20, -- Limit the number of simultaneous jobs. nil means no limit. Set to 20 in order to prevent PackerSync form being "stuck" -> https://github.com/wbthomason/packer.nvim/issues/746
-	-- Have packer use a popup window
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+  enable = true, -- enable profiling via :PackerCompile profile=true
+  threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+  max_jobs = 20, -- Limit the number of simultaneous jobs. nil means no limit. Set to 20 in order to prevent PackerSync form being "stuck" -> https://github.com/wbthomason/packer.nvim/issues/746
+  -- Have packer use a popup window
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
 })
 
 packer.startup(function(use)
-	-- actual plugins list
-	use("wbthomason/packer.nvim")
+  -- actual plugins list
+  use("wbthomason/packer.nvim")
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
-		config = get_config("telescope"),
-	})
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+    config = get_config("telescope"),
+  })
 
-	use({ "jvgrootveld/telescope-zoxide" })
-	use({ "crispgm/telescope-heading.nvim" })
-	use({ "nvim-telescope/telescope-symbols.nvim" })
-	use({ "nvim-telescope/telescope-file-browser.nvim" })
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use({ "nvim-telescope/telescope-packer.nvim" })
-	use({ "nvim-telescope/telescope-ui-select.nvim" })
+  use({ "jvgrootveld/telescope-zoxide" })
+  use({ "crispgm/telescope-heading.nvim" })
+  use({ "nvim-telescope/telescope-symbols.nvim" })
+  use({ "nvim-telescope/telescope-file-browser.nvim" })
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  use({ "nvim-telescope/telescope-packer.nvim" })
+  use({ "nvim-telescope/telescope-ui-select.nvim" })
 
-	use({ "kyazdani42/nvim-tree.lua", config = get_config("nvim-tree") })
+  use({ "kyazdani42/nvim-tree.lua", config = get_config("nvim-tree") })
 
-	use({ "numToStr/Navigator.nvim", config = get_config("navigator") })
+  use({ "numToStr/Navigator.nvim", config = get_config("navigator") })
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		config = get_config("lualine"),
-		event = "VimEnter",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+  use({
+    "nvim-lualine/lualine.nvim",
+    config = get_config("lualine"),
+    event = "VimEnter",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
 
-	use({ "windwp/nvim-autopairs", config = get_config("nvim-autopairs") })
+  use({ "windwp/nvim-autopairs", config = get_config("nvim-autopairs") })
 
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		config = get_config("treesitter"),
-		run = ":TSUpdate",
-	})
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    config = get_config("treesitter"),
+    run = ":TSUpdate",
+  })
 
-	use("nvim-treesitter/nvim-treesitter-textobjects")
+  use("nvim-treesitter/nvim-treesitter-textobjects")
 
-	use("RRethy/nvim-treesitter-endwise")
+  use("RRethy/nvim-treesitter-endwise")
 
-	use({
-		"hrsh7th/nvim-cmp",
-		requires = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"f3fora/cmp-spell",
-			"hrsh7th/cmp-calc",
-			"lukas-reineke/cmp-rg",
-			"hrsh7th/cmp-nvim-lsp-signature-help",
-		},
-		config = get_config("cmp"),
-	})
+  use({
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "f3fora/cmp-spell",
+      "hrsh7th/cmp-calc",
+      "lukas-reineke/cmp-rg",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+    },
+    config = get_config("cmp"),
+  })
 
-	use({ "rafamadriz/friendly-snippets" })
-	use({
-		"L3MON4D3/LuaSnip",
-		requires = "saadparwaiz1/cmp_luasnip",
-		config = get_config("luasnip"),
-	})
+  use({ "rafamadriz/friendly-snippets" })
+  use({
+    "L3MON4D3/LuaSnip",
+    requires = "saadparwaiz1/cmp_luasnip",
+    config = get_config("luasnip"),
+  })
 
-	-- requirement for Neogit
-	use({
-		"sindrets/diffview.nvim",
-		cmd = {
-			"DiffviewOpen",
-			"DiffviewClose",
-			"DiffviewToggleFiles",
-			"DiffviewFocusFiles",
-		},
-		config = get_config("diffview"),
-	})
+  -- requirement for Neogit
+  use({
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+    },
+    config = get_config("diffview"),
+  })
 
-	use({
-		"TimUntersberger/neogit",
-		requires = { "nvim-lua/plenary.nvim" },
-		cmd = "Neogit",
-		config = get_config("neogit"),
-	})
+  use({
+    "TimUntersberger/neogit",
+    requires = { "nvim-lua/plenary.nvim" },
+    cmd = "Neogit",
+    config = get_config("neogit"),
+  })
 
-	use({ "f-person/git-blame.nvim", config = get_config("git-blame") })
+  use({ "f-person/git-blame.nvim", config = get_config("git-blame") })
 
-	use({
-		"lewis6991/gitsigns.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = get_config("gitsigns"),
-	})
+  use({
+    "lewis6991/gitsigns.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = get_config("gitsigns"),
+  })
 
-	use({ "tpope/vim-fugitive" }) -- yeah this is not lua but one of the best Vim plugins ever
+  use({ "tpope/vim-fugitive" }) -- yeah this is not lua but one of the best Vim plugins ever
 
-	use("p00f/nvim-ts-rainbow")
+  use("p00f/nvim-ts-rainbow")
 
-	use({
-		"kevinhwang91/nvim-bqf",
-		requires = { { "junegunn/fzf", module = "nvim-bqf" }, config = get_config("nvim-bqf") },
-	})
+  use({
+    "kevinhwang91/nvim-bqf",
+    requires = { { "junegunn/fzf", module = "nvim-bqf" }, config = get_config("nvim-bqf") },
+  })
 
-	use({
-		"akinsho/nvim-bufferline.lua",
-		requires = "kyazdani42/nvim-web-devicons",
-		event = "BufReadPre",
-		config = get_config("bufferline"),
-	})
+  use({
+    "akinsho/nvim-bufferline.lua",
+    requires = "kyazdani42/nvim-web-devicons",
+    event = "BufReadPre",
+    config = get_config("bufferline"),
+  })
 
-	use("famiu/bufdelete.nvim")
+  use("famiu/bufdelete.nvim")
 
-	use({ "neovim/nvim-lspconfig", config = get_config("lsp") })
+  use({ "neovim/nvim-lspconfig", config = get_config("lsp") })
 
-	use({ "onsails/lspkind-nvim", requires = { "famiu/bufdelete.nvim" } })
+  use({ "onsails/lspkind-nvim", requires = { "famiu/bufdelete.nvim" } })
 
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-		config = get_config("null-ls"),
-	})
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    requires = { { "nvim-lua/plenary.nvim" } },
+    config = get_config("null-ls"),
+  })
 
-	use({
-		"simrat39/symbols-outline.nvim",
-		cmd = { "SymbolsOutline" },
-		config = get_config("symbols"),
-	})
+  use({
+    "simrat39/symbols-outline.nvim",
+    cmd = { "SymbolsOutline" },
+    config = get_config("symbols"),
+  })
 
-	use({
-		"lukas-reineke/indent-blankline.nvim",
-		event = "BufReadPre",
-		config = [[require("config/indent-blankline")]],
-	})
+  use({
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPre",
+    config = [[require("config/indent-blankline")]],
+  })
 
-	use({
-		"akinsho/nvim-toggleterm.lua",
-		keys = { "<C-n>", "<leader>fl" },
-		config = get_config("toggleterm"),
-	})
+  use({
+    "akinsho/nvim-toggleterm.lua",
+    keys = { "<C-n>", "<leader>fl" },
+    config = get_config("toggleterm"),
+  })
 
-	use({
-		"folke/todo-comments.nvim",
-		requires = "nvim-lua/plenary.nvim",
-		config = get_config("todo"),
-	})
+  use({
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = get_config("todo"),
+  })
 
-	use({ "ahmedkhalf/project.nvim", config = get_config("project") })
+  use({ "ahmedkhalf/project.nvim", config = get_config("project") })
 
-	use("ironhouzi/starlite-nvim")
+  use("ironhouzi/starlite-nvim")
 
-	use({ "folke/which-key.nvim", config = get_config("which-key") })
+  use({ "folke/which-key.nvim", config = get_config("which-key") })
 
-	use("junegunn/vim-easy-align") -- no lua alternative, https://github.com/Vonr/align.nvim not working for me
+  use("junegunn/vim-easy-align") -- no lua alternative, https://github.com/Vonr/align.nvim not working for me
 
-	use({ "rhysd/vim-grammarous", cmd = "GrammarousCheck" })
+  use({ "rhysd/vim-grammarous", cmd = "GrammarousCheck" })
 
-	use({ "RRethy/vim-illuminate" })
+  use({ "RRethy/vim-illuminate" })
 
-	use({
-		"ptzz/lf.vim",
-		requires = "voldikss/vim-floaterm",
-		config = get_config("lf"),
-	})
+  use({
+    "ptzz/lf.vim",
+    requires = "voldikss/vim-floaterm",
+    config = get_config("lf"),
+  })
 
-	if settings.theme == "nightfox" then
-		use({ "EdenEast/nightfox.nvim", config = get_config("nightfox") })
-	elseif settings.theme == "catppuccino" then
-		use({ "catppuccin/nvim", as = "catppuccin", config = get_config("catppuccin") })
-	else
-		use({ "catppuccin/nvim", as = "catppuccin", config = get_config("catppuccin") })
-	end
+  if settings.theme == "nightfox" then
+    use({ "EdenEast/nightfox.nvim", config = get_config("nightfox") })
+  elseif settings.theme == "catppuccino" then
+    use({ "catppuccin/nvim", as = "catppuccin", config = get_config("catppuccin") })
+  else
+    use({ "catppuccin/nvim", as = "catppuccin", config = get_config("catppuccin") })
+  end
 
-	use({
-		"ThePrimeagen/harpoon",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = get_config("harpoon"),
-	})
+  use({
+    "ThePrimeagen/harpoon",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = get_config("harpoon"),
+  })
 
-	use({ "folke/zen-mode.nvim", cmd = "ZenMode", config = get_config("zen-mode") })
+  use({ "folke/zen-mode.nvim", cmd = "ZenMode", config = get_config("zen-mode") })
 
-	use({ "folke/twilight.nvim", config = get_config("twilight") })
+  use({ "folke/twilight.nvim", config = get_config("twilight") })
 
-	use({ "tweekmonster/startuptime.vim" })
+  use({ "tweekmonster/startuptime.vim" })
 
-	use({ "ggandor/lightspeed.nvim" })
+  use({ "ggandor/lightspeed.nvim" })
 
-	use({ "ray-x/go.nvim", config = get_config("go"), ft = { "go" } })
+  use({ "ray-x/go.nvim", config = get_config("go"), ft = { "go" } })
 
-	use({ "LudoPinelli/comment-box.nvim", config = get_config("comment-box") })
+  use({ "LudoPinelli/comment-box.nvim", config = get_config("comment-box") })
 
-	use({ "rcarriga/nvim-notify", config = get_config("notify") })
+  use({ "rcarriga/nvim-notify", config = get_config("notify") })
 
-	use({ "echasnovski/mini.nvim", branch = "stable", config = get_config("mini") })
+  use({ "echasnovski/mini.nvim", branch = "stable", config = get_config("mini") })
 
-	use({
-		"https://gitlab.com/yorickpeterse/nvim-window.git",
-		config = get_config("nvim-window"),
-	})
+  use({
+    "https://gitlab.com/yorickpeterse/nvim-window.git",
+    config = get_config("nvim-window"),
+  })
 
-	use({
-		"waylonwalker/Telegraph.nvim",
-		config = function()
-			require("telegraph").setup({})
-		end,
-	})
+  use({
+    "waylonwalker/Telegraph.nvim",
+    config = function()
+      require("telegraph").setup({})
+    end,
+  })
 
-	use({ "rhysd/conflict-marker.vim" })
+  use({ "rhysd/conflict-marker.vim" })
 
-	use({ "edluffy/specs.nvim", config = get_config("specs") })
+  use({ "edluffy/specs.nvim", config = get_config("specs") })
 
-	use({ "mfussenegger/nvim-ts-hint-textobject" })
+  use({ "mfussenegger/nvim-ts-hint-textobject" })
 
-	use({
-		"goolord/alpha-nvim",
-		requires = { "kyazdani42/nvim-web-devicons" },
-		config = get_config("alpha-nvim"),
-	})
+  use({
+    "goolord/alpha-nvim",
+    requires = { "kyazdani42/nvim-web-devicons" },
+    config = get_config("alpha-nvim"),
+  })
 
-	use({ "SmiteshP/nvim-navic" })
+  use({ "SmiteshP/nvim-navic" })
 
-	use({
-		"j-hui/fidget.nvim",
-		config = function()
-			require("fidget").setup({})
-		end,
-	})
+  use({
+    "j-hui/fidget.nvim",
+    config = function()
+      require("fidget").setup({})
+    end,
+  })
 
-	use({
-		"kevinhwang91/nvim-ufo",
-		requires = "kevinhwang91/promise-async",
-		config = function()
-			require("ufo").setup()
-		end,
-	})
+  use({
+    "kevinhwang91/nvim-ufo",
+    requires = "kevinhwang91/promise-async",
+    config = function()
+      require("ufo").setup()
+    end,
+  })
 
-	use({
-		"https://gitlab.com/yorickpeterse/nvim-pqf.git",
-		config = function()
-			require("pqf").setup()
-		end,
-	})
+  use({
+    "https://gitlab.com/yorickpeterse/nvim-pqf.git",
+    config = function()
+      require("pqf").setup()
+    end,
+  })
 
-	use({ "Djancyp/cheat-sheet" })
+  use({ "Djancyp/cheat-sheet" })
 
-	use({ "vimpostor/vim-tpipeline" })
+  use({ "vimpostor/vim-tpipeline" })
 
-	use({
-		"anuvyklack/hydra.nvim",
-		requires = "anuvyklack/keymap-layer.nvim", -- needed only for pink hydras
-		config = get_config("hydra"),
-	})
+  use({
+    "anuvyklack/hydra.nvim",
+    requires = "anuvyklack/keymap-layer.nvim", -- needed only for pink hydras
+    config = get_config("hydra"),
+  })
 end)
 
 -- TODO: ????
