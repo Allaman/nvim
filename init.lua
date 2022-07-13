c52f4e4418e284ed57e53459995388d336c4eb0b
Michael <michaeljohannpeter@gmail.com>
2022-07-13 09:46:28 +0200

    fix https://github.com/neovim/neovim/pull/19216

--- init.lua
+++ init.lua
@@ -7,10 +7,3 @@ require("mappings")
 require("options")
 -- Vim autocommands/autogroups
 require("autocmd")
-
--- enable filetypee.lua
--- This feature is currently opt-in
--- as it does not yet completely match all of the filetypes covered by filetype.vim
-vim.g.do_filetype_lua = 1
--- disable filetype.vim
-vim.g.did_load_filetypes = 0
