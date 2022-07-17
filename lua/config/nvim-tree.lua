deb34a684a81c36fab7dd4a97097912ad54a8592
Michael <michaeljohannpeter@gmail.com>
2022-07-17 16:35:12 +0200

    fix nvim-tree indent-markers

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -73,9 +73,9 @@ require("nvim-tree").setup({
     indent_markers = {
       enable = true,
       icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
+        corner = "└",
+        edge = "│",
+        none = " ",
       },
     },
     icons = {
