3d9bcd4e439b898dba64f9ccbff622783eba995e
Michael <michaeljohannpeter@gmail.com>
2022-07-17 16:34:52 +0200

    add vim-illuminate configuration

---
+++ lua/config/illuminate.lua
@@ -0,0 +1,4 @@
+local g = vim.g
+g.Illuminate_ftblacklist = { "NvimTree", "alpha" } -- illuminate can also be disabled for various filetypes
+g.Illuminate_delay = 500 -- Time in milliseconds (default 0)
+g.Illuminate_highlightUnderCursor = 0 -- Don't highlight word under cursor (default: 1)
