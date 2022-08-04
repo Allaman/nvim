a75212a4b2a2ac76292dd7ce27eeb1403c82a588
Michael <michaeljohannpeter@gmail.com>
2022-08-04 20:02:40 +0200

    exclude more filetypes from illuminate

--- lua/config/illuminate.lua
+++ lua/config/illuminate.lua
@@ -1,4 +1,4 @@
 local g = vim.g
-g.Illuminate_ftblacklist = { "NvimTree", "alpha" } -- illuminate can also be disabled for various filetypes
+g.Illuminate_ftblacklist = { "NvimTree", "alpha", "packer", "qf", "Outline" } -- illuminate can also be disabled for various filetypes
 g.Illuminate_delay = 500 -- Time in milliseconds (default 0)
 g.Illuminate_highlightUnderCursor = 0 -- Don't highlight word under cursor (default: 1)
