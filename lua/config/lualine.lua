79f6b9e86fbde2953a2de8582f88e305f6b67809
Michael <michaeljohannpeter@gmail.com>
2022-08-06 22:11:08 +0200

    explicitly add lualine separator config

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -15,6 +15,8 @@ require("lualine").setup({
   options = {
     theme = "auto",
     icons_enabled = true,
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {},
     always_divide_middle = false,
   },
