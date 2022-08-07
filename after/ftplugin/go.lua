320ce1ac0ea7435687852bd55639ebc484f5f5d1
Michael <michaeljohannpeter@gmail.com>
2022-08-07 14:57:52 +0200

    improve golang mappings via go.nvim

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,20 +2,3 @@ local vo = vim.opt_local
 vo.tabstop = 4
 vo.shiftwidth = 4
 vo.softtabstop = 4
-
-local wk = require("which-key")
-local default_options = { silent = true }
-wk.register({
-  c = {
-    name = "Coding",
-    a = { "<cmd>GoAddTag<cr>", "Add tags to struct" },
-    c = { "<cmd>GoCoverage<cr>", "Test coverage" },
-    e = { "<cmd>GoIfErr<cr>", "Add if err" },
-    g = { "<cmd>lua require('go.comment').gen()<cr>", "Generate comment" },
-    l = { "<cmd>GoLint<cr>", "Run linter" },
-    r = { "<cmd>GoRun<cr>", "Run" },
-    s = { "<cmd>GoFillStruct<cr>", "Autofill struct" },
-    t = { "<cmd>GoTest<cr>", "Run tests" },
-    v = { "<cmd>GoVet<cr>", "Go vet" },
-  },
-}, { prefix = "<leader>", mode = "n", default_options })
--- lua/config/go.lua
+++ lua/config/go.lua
@@ -20,6 +20,50 @@ require("go").setup({
   lsp_gofumpt = false, -- true: set default gofmt in gopls format to gofumpt
   lsp_on_attach = function(client, bufnr)
     require("functions").custom_lsp_attach(client, bufnr)
+    local wk = require("which-key")
+    local default_options = { silent = true }
+    wk.register({
+      c = {
+        name = "Coding",
+        a = { "<cmd>GoCodeAction<cr>", "Code action" },
+        e = { "<cmd>GoIfErr<cr>", "Add if err" },
+        h = {
+          name = "Helper",
+          a = { "<cmd>GoAddTag<cr>", "Add tags to struct" },
+          r = { "<cmd>GoRMTag<cr>", "Remove tags to struct" },
+          c = { "<cmd>GoCoverage<cr>", "Test coverage" },
+          g = { "<cmd>lua require('go.comment').gen()<cr>", "Generate comment" },
+          v = { "<cmd>GoVet<cr>", "Go vet" },
+          t = { "<cmd>GoModTidy", "Go mod tidy" },
+          i = { "<cmd>GoModInit", "Go mod init" },
+        },
+        i = { "<cmd>GoToggleInlay<cr>", "Toggle inlay" },
+        l = { "<cmd>GoLint<cr>", "Run linter" },
+        o = { "<cmd>GoPkgOutline<cr>", "Outline" },
+        r = { "<cmd>GoRun<cr>", "Run" },
+        s = { "<cmd>GoFillStruct<cr>", "Autofill struct" },
+        t = {
+          name = "Tests",
+          r = { "<cmd>GoTest<cr>", "Run tests" },
+          a = { "<cmd>GoAlt!<cr>", "Open alt file" },
+          s = { "<cmd>GoAltS!<cr>", "Open alt file in split" },
+          v = { "<cmd>GoAltV!<cr>", "Open alt file in vertical split" },
+          u = { "<cmd>GoTestFunc<cr>", "Run test for current func" },
+          f = { "<cmd>GoTestFile<cr>", "Run test for current file" },
+        },
+        x = {
+          name = "Code Lens",
+          l = { "<cmd>GoCodeLenAct<cr>", "Toggle Lens" },
+          a = { "<cmd>GoCodeAction<cr>", "Code Action" },
+        },
+      },
+    }, { prefix = "<leader>", mode = "n", default_options })
+    wk.register({
+      c = {
+        name = "Coding",
+        j = { "<cmd>'<,'>GoJson2Struct<cr>", "Json to struct" },
+      },
+    }, { prefix = "<leader>", mode = "v", default_options })
   end, -- nil: use on_attach function defined in go/lsp.lua,
   --      when lsp_cfg is true
   -- if lsp_on_attach is a function: use this function as on_attach function for gopls
