b940ebc0c756086f663564ff16dba8286089d371
Michael <michaeljohannpeter@gmail.com>
2022-08-20 15:41:32 +0200

    fix wrong config value for nvim-tree

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -32,7 +32,7 @@ require("nvim-tree").setup({
     enable = true,
     -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
     -- only relevant when `update_focused_file.enable` is true
-    update_cwd = true,
+    update_root = true,
     -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
     -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
     ignore_list = { ".git", "node_modules", ".cache" },
