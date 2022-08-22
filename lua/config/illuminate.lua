0736fcc32821da3af56c365cf65905a6b4f56881
Michael <michaeljohannpeter@gmail.com>
2022-08-22 11:29:11 +0200

    add new option in illuminate

--- lua/config/illuminate.lua
+++ lua/config/illuminate.lua
@@ -7,6 +7,10 @@ require("illuminate").configure({
   },
   -- delay: delay in milliseconds
   delay = 500,
+  -- filetype_overrides: filetype specific overrides.
+  -- The keys are strings to represent the filetype while the values are tables that
+  -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+  filetype_overrides = {},
   -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
   filetypes_denylist = { "NvimTree", "alpha", "packer", "qf", "Outline", "fugitive" },
   -- filetypes_allowlist: filetypes to illuminate, this is overridden by filetypes_denylist
