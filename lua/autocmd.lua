679a84e88bd99c29ac135c55f37777bd02e622d5
Michael <michaeljohannpeter@gmail.com>
2022-09-05 10:04:18 +0200

    improve set spell autocmd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -86,7 +86,14 @@ vim.api.nvim_create_autocmd("User", {
 -- Enable spell checking for certain file types
 api.nvim_create_autocmd(
   { "BufRead", "BufNewFile" },
-  { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" }
+  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en,de"
+    end,
+  }
 )
 
 -- automatically run PackerSync on save of plugins.lua
