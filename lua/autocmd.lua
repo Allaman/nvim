6eaef1fcadb370ffa1bc0e7980501098d74d88c3
Michael <michaeljohannpeter@gmail.com>
2022-09-05 09:58:26 +0200

    move autocmd to autocmd.lua

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -57,6 +57,32 @@ api.nvim_create_autocmd(
   { pattern = "*", command = "set nocursorline", group = cursorGrp }
 )
 
+-- when there is no buffer left show Alpha dashboard
+vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BDeletePre",
+  group = "alpha_on_empty",
+  callback = function(event)
+    local found_non_empty_buffer = false
+    local buffers = require("functions").get_listed_buffers()
+
+    for _, bufnr in ipairs(buffers) do
+      if not found_non_empty_buffer then
+        local name = vim.api.nvim_buf_get_name(bufnr)
+        local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+
+        if bufnr ~= event.buf and name ~= "" and ft ~= "Alpha" then
+          found_non_empty_buffer = true
+        end
+      end
+    end
+
+    if not found_non_empty_buffer then
+      vim.cmd([[:Alpha]])
+    end
+  end,
+})
+
 -- Enable spell checking for certain file types
 api.nvim_create_autocmd(
   { "BufRead", "BufNewFile" },
--- lua/functions.lua
+++ lua/functions.lua
@@ -105,31 +105,7 @@ function M.bufdelete(bufnum)
   require("bufdelete").bufdelete(bufnum, true)
 end
 
--- when there is no buffer left show Alpha dashboard
-vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
-vim.api.nvim_create_autocmd("User", {
-  pattern = "BDeletePre",
-  group = "alpha_on_empty",
-  callback = function(event)
-    local found_non_empty_buffer = false
-    local buffers = M.get_listed_buffers()
 
-    for _, bufnr in ipairs(buffers) do
-      if not found_non_empty_buffer then
-        local name = vim.api.nvim_buf_get_name(bufnr)
-        local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
-
-        if bufnr ~= event.buf and name ~= "" and ft ~= "Alpha" then
-          found_non_empty_buffer = true
-        end
-      end
-    end
-
-    if not found_non_empty_buffer then
-      vim.cmd([[:Alpha]])
-    end
-  end,
-})
 
 function M.custom_lsp_attach(client, bufnr)
   -- disable formatting for LSP clients as this is handled by null-ls
