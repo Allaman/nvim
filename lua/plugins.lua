7ec2b0ed9e030a915bc56fd7d0eafe141957148e
Michael <michaeljohannpeter@gmail.com>
2022-10-06 20:50:19 +0200

    add some lazy-loading to certain plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -42,7 +42,8 @@ packer.startup(function(use)
 
   use({
     "nvim-telescope/telescope.nvim",
-    requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+    requires = { "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
     config = get_config("telescope"),
   })
 
@@ -57,6 +58,7 @@ packer.startup(function(use)
   use({
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
+    cmd = "NeoTree*",
     requires = {
       {
         -- only needed if you want to use the commands with "_with_window_picker" suffix
@@ -80,9 +82,9 @@ packer.startup(function(use)
     run = ":TSUpdate",
   })
 
-  use("nvim-treesitter/nvim-treesitter-textobjects")
+  use({ "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" })
 
-  use("RRethy/nvim-treesitter-endwise")
+  use({ "RRethy/nvim-treesitter-endwise", after = "nvim-treesitter" })
 
   use({ "David-Kunz/markid" })
 
@@ -136,7 +138,7 @@ packer.startup(function(use)
 
   use({ "tpope/vim-fugitive" }) -- yeah this is not lua but one of the best Vim plugins ever
 
-  use("p00f/nvim-ts-rainbow")
+  use({ "p00f/nvim-ts-rainbow", after = "nvim-treesitter" })
 
   use({
     "kevinhwang91/nvim-bqf",
@@ -178,6 +180,7 @@ packer.startup(function(use)
   -- TODO: switch to https://github.com/B4mbus/todo-comments.nvim ?
   use({
     "folke/todo-comments.nvim",
+    cmd = "TodoQuickFix",
     requires = "nvim-lua/plenary.nvim",
     config = get_config("todo"),
   })
@@ -203,7 +206,7 @@ packer.startup(function(use)
 
   use({ "ray-x/go.nvim", requires = "ray-x/guihua.lua", config = get_config("go"), ft = { "go" } })
 
-  use({ "LudoPinelli/comment-box.nvim", config = get_config("comment-box") })
+  use({ "LudoPinelli/comment-box.nvim", cmd = "CB*", config = get_config("comment-box") })
 
   use({ "rcarriga/nvim-notify", config = get_config("notify") })
 
@@ -261,6 +264,8 @@ packer.startup(function(use)
 
   use({
     "williamboman/mason.nvim",
+    cmd = "Mason*",
+    module = "mason-tool-installer",
     requires = { "williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim" },
     config = get_config("lsp.mason"),
   })
@@ -273,9 +278,6 @@ packer.startup(function(use)
       "rcarriga/nvim-dap-ui",
       "theHamsta/nvim-dap-virtual-text",
     },
-    config = function()
-      require("config.dap").setup()
-    end,
   })
 
   use({
@@ -305,6 +307,7 @@ packer.startup(function(use)
 
   use({
     "anuvyklack/windows.nvim",
+    event = "VimEnter",
     requires = {
       "anuvyklack/middleclass",
       "anuvyklack/animation.nvim",
@@ -312,7 +315,7 @@ packer.startup(function(use)
     config = get_config("windows"),
   })
 
-  use({ "axieax/urlview.nvim", config = get_config("urlview") })
+  use({ "axieax/urlview.nvim", cmd = "Urlview", config = get_config("urlview") })
   use({
     "folke/noice.nvim",
     event = "VimEnter",
