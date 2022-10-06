9a26b50902074eb5a99c6a10721e390dad2fa942
Michael <michaeljohannpeter@gmail.com>
2022-10-06 20:51:42 +0200

    remove mode section from mini statusline

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -83,7 +83,23 @@ require("mini.statusline").setup({
   -- `:h statusline` and code of default contents (used instead of `nil`).
   content = {
     -- Content for active window
-    active = nil,
+    active = function()
+      -- local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+      local git = MiniStatusline.section_git({ trunc_width = 75 })
+      local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+      local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 30 })
+      local location = MiniStatusline.section_location({ trunc_width = 75 })
+      return MiniStatusline.combine_groups({
+        -- { hl = mode_hl, strings = { mode } },
+        { hl = "MiniStatuslineDevinfo", strings = { git, diagnostics } },
+        "%<", -- Mark general truncate point
+        { hl = "MiniStatuslineFilename", strings = { filename } },
+        "%=", -- End left alignment
+        { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+        { hl = mode_hl, strings = { location } },
+      })
+    end,
     -- Content for inactive window(s)
     inactive = nil,
   },
