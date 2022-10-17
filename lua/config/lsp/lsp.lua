d91f379e06ad2c9524fde4cfe15565d418706284
Michael <michaeljohannpeter@gmail.com>
2022-10-17 16:18:12 +0200

    fix deprecation warning

--- lua/config/lsp/lsp.lua
+++ lua/config/lsp/lsp.lua
@@ -4,7 +4,7 @@ local opts = require("config.lsp.opts")
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- enable autoclompletion via nvim-cmp
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- enable folding capabilities for nvim-ufo
 capabilities.textDocument.foldingRange = {
   dynamicRegistration = false,
