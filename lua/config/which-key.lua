1d208cf2fbec93bb618320e362bb5b7fc76968e3
Michael <michaeljohannpeter@gmail.com>
2022-10-19 23:16:38 +0200

    tweak which-key look

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -7,7 +7,7 @@ require("which-key").setup({
     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     spelling = {
       enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-      suggestions = 20, -- how many suggestions should be shown in the list?
+      suggestions = 10, -- how many suggestions should be shown in the list?
     },
     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
     -- No actual key bindings are created
@@ -23,7 +23,7 @@ require("which-key").setup({
   },
   -- add operators that will trigger motion and text object completion
   -- to enable all native operators, set the preset / operators plugin above
-  operators = { gc = "Comments" },
+  operators = {},
   key_labels = {
     -- override the label used to display some keys. It doesn't effect WK in any other way.
     -- For example:
@@ -37,15 +37,15 @@ require("which-key").setup({
     group = "+", -- symbol prepended to a group
   },
   window = {
-    border = "single", -- none, single, double, shadow
+    border = "none", -- none, single, double, shadow
     position = "bottom", -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    margin = { 0, 40, 3, 40 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
   },
   layout = {
-    height = { min = 4, max = 25 }, -- min and max height of the columns
-    width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 2, -- spacing between columns
+    height = { min = 3, max = 25 }, -- min and max height of the columns
+    width = { min = 10, max = 50 }, -- min and max width of the columns
+    spacing = 20, -- spacing between columns
     align = "center", -- align columns left, center or right
   },
   ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
@@ -58,6 +58,7 @@ require("which-key").setup({
     "call",
     "lua",
     "require",
+    "Plug",
     "^:",
     "^ ",
   }, -- hide mapping boilerplate
