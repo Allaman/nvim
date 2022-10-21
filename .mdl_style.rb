e1d8596f05d58e49be13c0f4ec7c78980dc1d5c7
Michael <michaeljohannpeter@gmail.com>
2022-10-21 14:50:47 +0200

    ignore markdownlint rules

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -5,3 +5,9 @@ rule 'MD013', :line_length => 99999
 
 # allow inline HTML
 exclude_rule "MD033"
+
+# First header should be a top level header
+exclude_rule "MD002"
+
+# First line in file should be a top level header
+exclude_rule "MD041"
