6506434b4a9fd711412efed47e1831377874144e
Michael <michaeljohannpeter@gmail.com>
2022-10-25 20:53:36 +0200

    telescope file_browser don't hijack netrw

--- lua/config/ui/telescope.lua
+++ lua/config/ui/telescope.lua
@@ -17,7 +17,7 @@ telescope.setup({
     },
     file_browser = {
       theme = "ivy",
-      hijack_netrw = true,
+      hijack_netrw = false,
       hidden = true,
       mappings = {
         i = {
