7b0b09350053ac0864ba5aef600befc1f4bd2901
Michael <michaeljohannpeter@gmail.com>
2022-11-04 18:15:57 +0100

    add date-time snippet for hugo / go

--- snippets/all.lua
+++ snippets/all.lua
@@ -11,4 +11,5 @@ return {
   env.parse("sehrg", "Sehr geehrte Damen und Herren, \n\n"),
   env.s("date", env.p(os.date, "%Y-%m-%d")),
   env.s("time", env.p(os.date, "%H:%M")),
+  env.s("htime", env.p(os.date, "%Y-%m-%dT%H:%M:%S+10:00")),
 }
