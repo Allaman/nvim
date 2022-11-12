6c280804616adb4462b97273e92a0a15b781cec2
Michael <michaeljohannpeter@gmail.com>
2022-11-12 02:10:43 +0100

    feat: comment out markdown link highlightning

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
 vim.opt_local.wrap = true
 
 -- match and highlight hyperlinks
-vim.cmd("hi matchURL guifg=DodgerBlue")
-vim.fn.matchadd("matchURL", [[http[s]\?:\/\/[[:alnum:]%\/_#.-]*]])
+-- vim.cmd("hi matchURL guifg=DodgerBlue")
+-- vim.fn.matchadd("matchURL", [[http[s]\?:\/\/[[:alnum:]%\/_#.-]*]])
