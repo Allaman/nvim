2afa62852a54a9357d2720ac36e81b9f54ab433e
Michael <michaeljohannpeter@gmail.com>
2022-12-06 21:22:40 +0100

    feat!: disable neotree as netrw replacement neotree is lazy loaded and therefore not loaded at start to replace netrw

--- lua/anvim/config/ui/neotree.lua
+++ lua/anvim/config/ui/neotree.lua
@@ -360,7 +360,7 @@ require("neo-tree").setup({
     search_limit = 50, -- max number of search results when using filters
     follow_current_file = false, -- This will find and focus the file in the active buffer every time
     -- the current file is changed while the tree is open.
-    hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree in whatever position is specified in window.position
+    hijack_netrw_behavior = "disabled", -- netrw disabled, opening a directory opens neo-tree in whatever position is specified in window.position
     -- "open_current",-- netrw disabled, opening a directory opens within the window like netrw would, regardless of window.position
     -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
     use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
