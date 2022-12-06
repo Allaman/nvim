d9a071c3e74a027561cd9d6f37b13f84f81eeafa
Michael <michaeljohannpeter@gmail.com>
2022-12-06 21:20:36 +0100

    refactor: fix module paths

--- lua/anvim/config/lsp/null-ls.lua
+++ lua/anvim/config/lsp/null-ls.lua
@@ -24,7 +24,7 @@ nls.setup({
     local default_options = { silent = true }
     wk.register({
       m = {
-        F = { "<cmd>lua require('config.lsp.utils').toggle_autoformat()<cr>", "Toggle format on save" },
+        F = { "<cmd>lua require('anvim.config.lsp.utils').toggle_autoformat()<cr>", "Toggle format on save" },
       },
     }, { prefix = "<leader>", mode = "n", default_options })
     if client.supports_method("textDocument/formatting") then
