6e57fb469d6da5e250af923c9aabb5eb7275850d
Michael <michaeljohannpeter@gmail.com>
2023-01-02 21:50:27 +0100

    feat: add oil.nvim

---
+++ lua/core/plugins/oil.lua
@@ -0,0 +1,29 @@
+local M = {
+  "stevearc/oil.nvim",
+}
+
+function M.config()
+  require("oil").setup({
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
+    },
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-v>"] = "actions.select_vsplit",
+      ["<C-h>"] = "actions.select_split",
+      ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = "actions.close",
+      ["-"] = "actions.parent",
+      ["_"] = "actions.open_cwd",
+      ["`"] = "actions.cd",
+      ["~"] = "actions.tcd",
+      ["."] = "actions.toggle_hidden",
+    },
+  })
+end
+
+return M
