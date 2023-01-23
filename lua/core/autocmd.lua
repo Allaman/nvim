717c6d10f4d73e5040f862a1252590ab610f0119
Michael <michaeljohannpeter@gmail.com>
2023-01-24 00:03:36 +0100

    refactor: use lua APIs in autocmds

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,5 +1,4 @@
 local api = vim.api
-local settings = require("core.settings")
 
 --- Remove all trailing whitespace on save
 local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
@@ -25,20 +24,41 @@ api.nvim_create_autocmd("Filetype", {
 })
 
 -- Highlight on yank
-local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
 api.nvim_create_autocmd("TextYankPost", {
-  command = "silent! lua vim.highlight.on_yank()",
-  group = yankGrp,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
+
 -- go to last loc when opening a buffer
-api.nvim_create_autocmd(
-  "BufReadPost",
-  { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
-)
+api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- windows to close with "q"
 api.nvim_create_autocmd("FileType", {
-  pattern = { "help", "startuptime", "qf", "fugitive", "null-ls-info", "dap-float" },
-  command = [[nnoremap <buffer><silent> q :close<CR>]],
+  pattern = {
+    "dap-float",
+    "fugitive",
+    "help",
+    "man",
+    "notify",
+    "null-ls-info",
+    "qf",
+    "PlenaryTestPopup",
+    "startuptime",
+    "tsplayground",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
 })
 api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
 
