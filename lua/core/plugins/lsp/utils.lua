5aeef115050a09a715ad8394ae4bdc4bc025dbfe
Michael <michaeljohannpeter@gmail.com>
2023-03-04 00:27:22 +0100

    fix: set correct log level in notification

--- lua/core/plugins/lsp/utils.lua
+++ lua/core/plugins/lsp/utils.lua
@@ -6,7 +6,11 @@ VIRTUAL_TEXT_ACTIVE = true
 -- toggle displaying virtual text
 M.toggle_virtual_text = function()
   VIRTUAL_TEXT_ACTIVE = not VIRTUAL_TEXT_ACTIVE
-  utils.notify(string.format("Virtualtext %s", VIRTUAL_TEXT_ACTIVE and "on" or "off"), 1, "lsp/utils.lua")
+  utils.notify(
+    string.format("Virtualtext %s", VIRTUAL_TEXT_ACTIVE and "on" or "off"),
+    vim.log.levels.INFO,
+    "lsp/utils.lua"
+  )
   vim.diagnostic.show(nil, 0, nil, { virtual_text = VIRTUAL_TEXT_ACTIVE })
 end
 
@@ -15,9 +19,9 @@ AUTOFORMAT_ACTIVE = true
 -- toggle null-ls's autoformatting
 M.toggle_autoformat = function()
   AUTOFORMAT_ACTIVE = not AUTOFORMAT_ACTIVE
-  require("core.utils.functions").notify(
+  utils.notify(
     string.format("Autoformatting %s", AUTOFORMAT_ACTIVE and "on" or "off"),
-    1,
+    vim.log.levels.INFO,
     "lsp.utils"
   )
 end
