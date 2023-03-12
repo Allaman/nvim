85d72ecdb64d03349da18af93e8749c08745159c
Michael <michaeljohannpeter@gmail.com>
2023-03-12 19:22:28 +0100

    fix: toggleterm new default value

--- lua/core/plugins/toggleterm.lua
+++ lua/core/plugins/toggleterm.lua
@@ -21,6 +21,7 @@ local M = {
       start_in_insert = true,
       insert_mappings = true, -- whether or not the open mapping applies in insert mode
       persist_size = true,
+      persist_mode = false,
       direction = "vertical", -- 'vertical' | 'horizontal' | 'window' | 'float',
       close_on_exit = true, -- close the terminal window when the process exits
       shell = vim.o.shell, -- change the default shell
