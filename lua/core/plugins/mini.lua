166c498940ae010fe0e15c1b0eca407ecfcaea5c
Michael <michaeljohannpeter@gmail.com>
2023-06-08 22:19:17 +0200

    style: remove obsolet commented out code

--- lua/core/plugins/mini.lua
+++ lua/core/plugins/mini.lua
@@ -74,58 +74,9 @@ local M = {
       },
     })
 
-    -- require("mini.statusline").setup({
-    --   -- Content of statusline as functions which return statusline string. See
-    --   -- `:h statusline` and code of default contents (used instead of `nil`).
-    --   content = {
-    --     -- Content for active window
-    --     active = function()
-    --       -- local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-    --       local git = MiniStatusline.section_git({ trunc_width = 75 })
-    --       local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-    --       local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-    --       local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 30 })
-    --       local location = MiniStatusline.section_location({ trunc_width = 75 })
-    --       return MiniStatusline.combine_groups({
-    --         -- { hl = mode_hl, strings = { mode } },
-    --         { hl = "MiniStatuslineDevinfo", strings = { git, diagnostics } },
-    --         "%<", -- Mark general truncate point
-    --         { hl = "MiniStatuslineFilename", strings = { filename } },
-    --         "%=", -- End left alignment
-    --         { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-    --         { hl = mode_hl, strings = { location } },
-    --       })
-    --     end,
-    --     -- Content for inactive window(s)
-    --     inactive = nil,
-    --   },
-    --
-    --   -- Whether to use icons by default
-    --   use_icons = true,
-    --
-    --   -- Whether to set Vim's settings for statusline (make it always shown with
-    --   -- 'laststatus' set to 2). To use global statusline in Neovim>=0.7.0, set
-    --   -- this to `false` and 'laststatus' to 3.
-    --   set_vim_settings = false,
-    -- })
-
     require("mini.align").setup()
     require("mini.test").setup()
 
-    -- TODO: unfortunately, mini does not support listing all TODOs,.. project-wiede
-    -- local hipatterns = require("mini.hipatterns")
-    -- hipatterns.setup({
-    --   highlighters = {
-    --     -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-    --     fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-    --     hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-    --     todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-    --     note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-    --
-    --     -- Highlight hex color strings (`#rrggbb`) using that color
-    --     hex_color = hipatterns.gen_highlighter.hex_color(),
-    --   },
-    -- })
   end,
 }
 
