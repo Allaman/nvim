1b852fe3de9162f0ef62c6b99909335fa5d7e806
Michael <michaeljohannpeter@gmail.com>
2023-06-08 22:29:23 +0200

    fix: remove unnecessary event

--- lua/core/plugins/go.lua
+++ lua/core/plugins/go.lua
@@ -3,7 +3,6 @@ local M = {
   dependencies = {
     "ray-x/guihua.lua",
   },
-  event = { "CmdlineEnter" },
   ft = { "go", "gomod" },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   config = function()
