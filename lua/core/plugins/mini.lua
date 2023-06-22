183cf1a9135cf9ac67ce8f728171174f6b2d2d01
Michael <michaeljohannpeter@gmail.com>
2023-06-22 22:53:52 +0200

    feat!: replace oil.nvim with mini.files

--- lua/core/plugins/mini.lua
+++ lua/core/plugins/mini.lua
@@ -111,4 +111,47 @@ return {
       }
     end,
   },
+
+  {
+    "echasnovski/mini.files",
+    event = { "VimEnter" },
+    opts = {
+      windows = {
+        -- Whether to show preview of directory under cursor
+        preview = true,
+      },
+    },
+    config = function(_, opts)
+      local show_dotfiles = true
+      local filter_show = function()
+        return true
+      end
+      local filter_hide = function(fs_entry)
+        return not vim.startswith(fs_entry.name, ".")
+      end
+      local toggle_dotfiles = function()
+        show_dotfiles = not show_dotfiles
+        local new_filter = show_dotfiles and filter_show or filter_hide
+        MiniFiles.refresh({ content = { filter = new_filter } })
+      end
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          -- Tweak left-hand side of mapping to your liking
+          vim.keymap.set("n", "H", toggle_dotfiles, { desc = "Toggle hidden files", buffer = buf_id })
+        end,
+      })
+      require("mini.files").setup(opts)
+    end,
+    keys = {
+      {
+        "-",
+        function()
+          MiniFiles.open()
+        end,
+        desc = "Open Mini Files",
+      },
+    },
+  },
 }
