328035f23b9080e24c4b21a328a31fe350376500
Michael <michaeljohannpeter@gmail.com>
2023-07-15 21:46:01 +0200

    feat: update neo-tree to v3.x branch

--- lua/core/plugins/neo-tree.lua
+++ lua/core/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 local M = {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
+  branch = "v3.x",
   cmd = "Neotree",
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -25,7 +25,6 @@ local M = {
     -- popup_border_style is for input and confirmation dialogs.
     -- Configurtaion of floating window is done in the individual source sections.
     -- "NC" is a special style that works well with NormalNC set
-    close_floats_on_escape_key = true,
     default_source = "filesystem",
     enable_diagnostics = true,
     enable_git_status = true,
@@ -372,7 +371,9 @@ local M = {
       --end,
       group_empty_dirs = false, -- when true, empty folders will be grouped together
       search_limit = 50, -- max number of search results when using filters
-      follow_current_file = false, -- This will find and focus the file in the active buffer every time
+      follow_current_file = {
+        enabled = false, -- This will find and focus the file in the active buffer every time
+      },
       -- the current file is changed while the tree is open.
       hijack_netrw_behavior = "disabled", -- netrw disabled, opening a directory opens neo-tree in whatever position is specified in window.position
       -- "open_current",-- netrw disabled, opening a directory opens within the window like netrw would, regardless of window.position
@@ -382,7 +383,9 @@ local M = {
     },
     buffers = {
       bind_to_cwd = true,
-      follow_current_file = true, -- This will find and focus the file in the active buffer every time
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+      },
       -- the current file is changed while the tree is open.
       group_empty_dirs = true, -- when true, empty directories will be grouped together
       window = {
