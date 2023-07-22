b2c31a29c564e9ad072db8dedc83590bc1b48b12
Mathis Albertzard <malbertzard@gmail.com>
2023-07-22 22:34:39 +0200

    refactor: Move Theme logic to lockup table

--- lua/core/plugins/theme.lua
+++ lua/core/plugins/theme.lua
@@ -1,48 +1,48 @@
 local settings = require("core.settings")
 
-if settings.theme == "nightfox" then
-  return {
-    "EdenEast/nightfox.nvim",
-    config = function()
-      require("core.plugins.themes.nightfox")
-    end,
-  }
-elseif settings.theme == "tundra" then
-  return {
-    "sam4llis/nvim-tundra",
-    config = function()
-      require("core.plugins.themes.tundra")
-    end,
-  }
-elseif settings.theme == "tokyonight" then
-  return {
-    "folke/tokyonight.nvim",
-    branch = "main",
-    config = function()
-      require("core.plugins.themes.tokyonight")
-    end,
-  }
-elseif settings.theme == "kanagawa" then
-  return {
-    "rebelot/kanagawa.nvim",
-    config = function()
-      require("lua.core.plugins.themes.kanagawa")
-    end,
-  }
-elseif settings.theme == "oxocarbon" then
-  return {
-    "nyoom-engineering/oxocarbon.nvim",
-    config = function()
-      vim.opt.background = "dark" -- set this to dark or light
-      vim.cmd("colorscheme oxocarbon")
-    end,
-  }
-else
-  return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-      require("core.plugins.themes.catppuccin")
-    end,
-  }
-end
+local themes = {
+    nightfox = {
+        "EdenEast/nightfox.nvim",
+        config = function()
+            require("core.plugins.themes.nightfox")
+        end
+    },
+    tundra = {
+        "sam4llis/nvim-tundra",
+        config = function()
+            require("core.plugins.themes.tundra")
+        end
+    },
+    tokyonight = {
+        "folke/tokyonight.nvim",
+        branch = "main",
+        config = function()
+            require("core.plugins.themes.tokyonight")
+        end
+    },
+    kanagawa = {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require("lua.core.plugins.themes.kanagawa")
+        end
+    },
+    oxocarbon = {
+        "nyoom-engineering/oxocarbon.nvim",
+        config = function()
+            vim.opt.background = "dark" -- set this to dark or light
+            vim.cmd("colorscheme oxocarbon")
+        end
+    },
+    default = {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            require("core.plugins.themes.catppuccin")
+        end
+    }
+}
+
+local selectedTheme = themes[settings.theme] or themes["default"]
+
+return selectedTheme
+
