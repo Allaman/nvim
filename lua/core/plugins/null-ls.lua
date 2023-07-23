c965bf7b5749071713d14c7f47026e9b527d8e7e
Michael <michaeljohannpeter@gmail.com>
2023-07-23 22:28:59 +0200

    docs: Add reminder that null-ls is deprecated

--- lua/core/plugins/null-ls.lua
+++ lua/core/plugins/null-ls.lua
@@ -1,3 +1,6 @@
+-- TODO: DEPRECATED https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1621
+-- https://github.com/mattn/efm-langserver
+-- https://github.com/tsuyoshicho/vim-efm-langserver-settings/blob/28b5535ca731233ee6e88bab2102610600c2b615/config/efm-langserver/config.yaml
 local M = {
   "jose-elias-alvarez/null-ls.nvim",
   dependencies = {
