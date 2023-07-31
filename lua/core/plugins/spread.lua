7d351eecebbf3d4a91db9cdd48ccb334fb13767a
Michael <michaeljohannpeter@gmail.com>
2023-07-31 15:12:32 +0200

    refactor: improve lazy config for spread.nvim

--- lua/core/plugins/spread.lua
+++ lua/core/plugins/spread.lua
@@ -1,22 +1,18 @@
--- TODO: alternative: https://github.com/Wansmer/treesj
-local M = {
+return {
   "aarondiel/spread.nvim",
-  event = "InsertEnter",
-}
-
-function M.config()
-  local spread = require("spread")
-  local wk = require("which-key")
-  local default_options = { silent = true }
-  wk.register({
-    m = {
-      S = {
-        name = "Spread",
-        c = { spread.combine, "Combine" },
-        s = { spread.out, "Split" },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local spread = require("spread")
+    local wk = require("which-key")
+    local default_options = { silent = true }
+    wk.register({
+      m = {
+        S = {
+          name = "Spread",
+          c = { spread.combine, "Combine" },
+          s = { spread.out, "Split" },
+        },
       },
-    },
-  }, { prefix = "<leader>", mode = "n", default_options })
-end
-
-return M
+    }, { prefix = "<leader>", mode = "n", default_options })
+  end,
+}
