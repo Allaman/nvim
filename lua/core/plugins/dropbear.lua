157c07044d30c70779d4fc05670385a2b11932da
Michael <michaeljohannpeter@gmail.com>
2023-10-03 12:02:34 +0200

    fix: pin dropbar.nvim to last working commit

--- lua/core/plugins/dropbear.lua
+++ lua/core/plugins/dropbear.lua
@@ -2,6 +2,6 @@ local utils = require("core.utils.functions")
 
 return {
   "Bekaboo/dropbar.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  commit = "3daffc1215d715a4e9c544e2c71db16aab61d86f",
   enabled = utils.isNeovimVersionsatisfied(10),
 }
