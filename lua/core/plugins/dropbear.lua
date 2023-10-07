2dcb981916a2b92d6f2a6bade370b793033fe8b5
Michael <michaeljohannpeter@gmail.com>
2023-10-08 00:02:27 +0200

    fix: dropbar by upgrading Neovim to latest HEAD

--- lua/core/plugins/dropbear.lua
+++ lua/core/plugins/dropbear.lua
@@ -2,6 +2,5 @@ local utils = require("core.utils.functions")
 
 return {
   "Bekaboo/dropbar.nvim",
-  commit = "3daffc1215d715a4e9c544e2c71db16aab61d86f",
   enabled = utils.isNeovimVersionsatisfied(10),
 }
