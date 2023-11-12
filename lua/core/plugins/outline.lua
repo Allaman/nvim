af5a1a205ca78e69c646acba4301d7941274cedd
Michael <michaeljohannpeter@gmail.com>
2023-11-12 13:10:16 +0100

    feat: Replace symbols-outline.nvim with outline.nvim See https://www.reddit.com/r/neovim/comments/17sq6hu/forking_symbolsoutlinenvim/

---
+++ lua/core/plugins/outline.lua
@@ -0,0 +1,15 @@
+local M = {
+  "hedyhli/outline.nvim",
+  cmd = { "Outline", "OutlineOpen" },
+  opts = {
+    symbol_folding = {
+      -- Depth past which nodes will be folded by default
+      autofold_depth = 1,
+    },
+  },
+  keys = {
+    { "<leader>tO", "<cmd>Outline<cr>", desc = "Toggle Outline" },
+  },
+}
+
+return M
