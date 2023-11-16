9caf2e0b82ca15400eb349cff1cdad92e972db72
Michael <michaeljohannpeter@gmail.com>
2023-11-16 21:11:36 +0100

    fix!: Remove unclear lua_ls config

--- lua/core/plugins/lsp/settings/lua_ls.lua
+++ lua/core/plugins/lsp/settings/lua_ls.lua
@@ -1,33 +1,33 @@
 -- https://github.com/sumneko/lua-language-server/blob/master/locale/en-us/setting.lua
 local opts = {
-  cmd = { "lua-language-server" },
+  -- cmd = { "lua-language-server" },
   format = {
     enable = false, -- let null-ls handle the formatting
   },
-  filetypes = { "lua" },
-  runtime = {
-    version = "LuaJIT",
-    path = vim.split(package.path, ";"),
-  },
-  completion = { enable = true, callSnippet = "Replace" },
-  diagnostics = {
-    -- enable = true,
-    -- globals = { "vim", "describe" },
-    globals = {
-      "vim",
-      "nnoremap",
-      "vnoremap",
-      "inoremap",
-      "tnoremap",
-      "use",
-    },
-  },
-  workspace = {
-    checkThirdParty = false,
-    -- adjust these two values if your performance is not optimal
-    maxPreload = 2000,
-    preloadFileSize = 1000,
-  },
+  -- filetypes = { "lua" },
+  -- runtime = {
+  --   version = "LuaJIT",
+  --   path = vim.split(package.path, ";"),
+  -- },
+  -- completion = { enable = true, callSnippet = "Replace" },
+  -- diagnostics = {
+  --   -- enable = true,
+  --   -- globals = { "vim", "describe" },
+  --   globals = {
+  --     "vim",
+  --     "nnoremap",
+  --     "vnoremap",
+  --     "inoremap",
+  --     "tnoremap",
+  --     "use",
+  --   },
+  -- },
+  -- workspace = {
+  --   checkThirdParty = false,
+  --   -- adjust these two values if your performance is not optimal
+  --   maxPreload = 2000,
+  --   preloadFileSize = 1000,
+  -- },
   telemetry = { enable = false },
 }
 
