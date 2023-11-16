fc96b8305d518b14e8a5916c5a6ee60c84e55664
Michael <michaeljohannpeter@gmail.com>
2023-11-16 21:12:02 +0100

    fix: Add neo-tree integration in project.nvim

--- lua/core/plugins/project.lua
+++ lua/core/plugins/project.lua
@@ -3,6 +3,13 @@ local M = {
   -- can't use 'opts' because module has non standard name 'project_nvim'
   config = function()
     require("project_nvim").setup({
+      -- neo-tree integration
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
       patterns = {
         ".git",
         "package.json",
