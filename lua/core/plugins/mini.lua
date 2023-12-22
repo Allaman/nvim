1a280e0f16ea1b5e2d9725a072c7daa8d2564f5d
Michael <michaeljohannpeter@gmail.com>
2023-12-22 23:28:34 +0100

    refactor: Remove default settings (mini.surround)

--- lua/core/plugins/mini.lua
+++ lua/core/plugins/mini.lua
@@ -15,20 +15,6 @@ return {
     opts = {
       -- Number of lines within which surrounding is searched
       n_lines = 50,
-
-      -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
-      highlight_duration = 500,
-
-      -- Module mappings. Use `''` (empty string) to disable one.
-      mappings = {
-        add = "sa", -- Add surrounding
-        delete = "sd", -- Delete surrounding
-        find = "sf", -- Find surrounding (to the right)
-        find_left = "sF", -- Find surrounding (to the left)
-        highlight = "sh", -- Highlight surrounding
-        replace = "sr", -- Replace surrounding
-        update_n_lines = "sn", -- Update `n_lines`
-      },
     },
     config = function(_, opts)
       vim.keymap.set(
