e50303e268c1e459b6b429c7367a1663e8b5f61f
Michael <michaeljohannpeter@gmail.com>
2024-01-20 11:39:55 +0100

    refactor: Move windows.lua to hydra dependencies

--- lua/core/plugins/hydra.lua
+++ lua/core/plugins/hydra.lua
@@ -3,6 +3,34 @@ local M = {
   event = "VeryLazy",
   dependencies = {
     "anuvyklack/keymap-layer.nvim",
+    {
+      -- for maximize mapping
+      "anuvyklack/windows.nvim",
+      lazy = true,
+      dependencies = {
+        "anuvyklack/middleclass",
+        "anuvyklack/animation.nvim",
+      },
+      opts = {
+        autowidth = { -- |windows.autowidth|
+          enable = false,
+          winwidth = 5, -- |windows.winwidth|
+          filetype = { -- |windows.autowidth.filetype|
+            help = 2,
+          },
+        },
+        ignore = { -- |windows.ignore|
+          buftype = { "quickfix" },
+          filetype = { "NvimTree", "neo-tree", "undotree", "NeogitStatus" },
+        },
+        animation = {
+          enable = false,
+          duration = 300,
+          fps = 30,
+          easing = "in_out_sine",
+        },
+      },
+    },
   },
   config = function()
     require("core.plugins.hydra.hydra")
