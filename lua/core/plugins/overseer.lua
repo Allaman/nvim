a57948b086dc2e364e10ad6719ad9088f1681e2b
Michael <michaeljohannpeter@gmail.com>
2024-01-31 22:14:46 +0100

    feat!: Unmap oversser mapping conflicts with window right/left

--- lua/core/plugins/overseer.lua
+++ lua/core/plugins/overseer.lua
@@ -5,6 +5,13 @@ return {
     { "<leader>r", "<cmd>OverseerRun<cr>", desc = "Overseer Run" },
     { "<leader>tr", "<cmd>OverseerToggle<cr>", desc = "Overseer" },
   },
-  opts = {},
+  opts = {
+    task_list = {
+      bindings = {
+        ["<C-l>"] = false,
+        ["<C-h>"] = false,
+      },
+    },
+  },
   config = true,
 }
