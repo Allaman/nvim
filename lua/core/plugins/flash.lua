9ffd8d0b1d5470967fd656d4abb0e9bf3c8ba48d
Michael <michaeljohannpeter@gmail.com>
2024-02-02 21:31:00 +0100

    feat: Use ss to jump to any word

--- lua/core/plugins/flash.lua
+++ lua/core/plugins/flash.lua
@@ -1,13 +1,25 @@
 return {
   "folke/flash.nvim",
-  event = { "BufReadPost", "BufNewFile" },
   opts = {},
   keys = {
     {
       "ss",
       mode = { "n", "x", "o" },
+      -- Jump to any word
       function()
-        require("flash").jump()
+        require("flash").jump({
+          pattern = ".", -- initialize pattern with any char
+          search = {
+            mode = function(pattern)
+              -- remove leading dot
+              if pattern:sub(1, 1) == "." then
+                pattern = pattern:sub(2)
+              end
+              -- return word pattern and proper skip pattern
+              return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+            end,
+          },
+        })
       end,
       desc = "Flash",
     },
