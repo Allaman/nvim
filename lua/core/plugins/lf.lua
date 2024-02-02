a2619b1ef0e62774d29c002baee15efad60e631c
Michael <michaeljohannpeter@gmail.com>
2024-02-02 22:00:22 +0100

    fix: Lf.nvim lazy config

--- lua/core/plugins/lf.lua
+++ lua/core/plugins/lf.lua
@@ -5,18 +5,18 @@ return {
   dependencies = "akinsho/nvim-toggleterm.lua",
   opts = {
     border = "rounded",
+  },
+  config = function(_, opts)
+    -- does not work when lazy loaded
+    vim.g.lf_netrw = 1
     -- TODO: my mapping <leader>lf is soo good but in the LSP cluster
-    config.key_mappings(),
+    config.key_mappings()
     vim.api.nvim_create_autocmd("User", {
       pattern = "LfTermEnter",
       callback = function(a)
         vim.api.nvim_buf_set_keymap(a.buf, "t", "q", "q", { nowait = true })
       end,
-    }),
-  },
-  config = function(opts)
-    -- does not work when lazy loaded
-    vim.g.lf_netrw = 1
+    })
     require("lf").setup(opts)
   end,
 }
