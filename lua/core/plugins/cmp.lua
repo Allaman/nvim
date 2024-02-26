14653cf27f8d445ed6e12564997df5209d95c62d
Michael <michaeljohannpeter@gmail.com>
2024-02-26 20:48:45 +0100

    feat!: Remove cmp-cmdline noice.nvim offers this functionality, too. Now nvim-cmp can also be lazy loaded only when entering insert mode

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -1,10 +1,10 @@
 local M = {
   "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-calc",
     "lukas-reineke/cmp-rg",
     "saadparwaiz1/cmp_luasnip",
@@ -106,24 +106,6 @@ local M = {
         end, { "i", "s" }),
       },
       sources = sources,
-    })
-
-    -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        { name = "cmdline" },
-      }),
     })
   end,
 }
