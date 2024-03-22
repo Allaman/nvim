1c0ada999854f3b8f2b7f10e8fcdfa356888599d
Michael <michaeljohannpeter@gmail.com>
2024-03-22 20:44:20 +0100

    fix(chatgpt): Improve lazy loading and naming

--- lua/core/plugins/chatgpt.lua
+++ lua/core/plugins/chatgpt.lua
@@ -1,9 +1,12 @@
-local conf = vim.g.config.plugins.chatgpt
+local config = vim.g.config.plugins.chatgpt
 return {
   "jackMort/ChatGPT.nvim",
-  enabled = conf.enable,
-  cmd = "ChatGPT",
-  opts = conf.opts,
+  enabled = config.enable,
+  cmd = { "ChatGPT", "ChatGPTRun" },
+  opts = config.opts,
+  config = function(_, opts)
+    require("chatgpt").setup(opts)
+  end,
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
