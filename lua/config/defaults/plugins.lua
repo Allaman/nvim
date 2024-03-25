0c44c40a956698a410ee9aafb5008017c569feb1
Michael <michaeljohannpeter@gmail.com>
2024-03-26 00:07:35 +0100

    refactor(harpoon): new config structure

--- lua/config/defaults/plugins.lua
+++ lua/config/defaults/plugins.lua
@@ -41,30 +41,6 @@ return {
       -- https://github.com/olexsmir/gopher.nvim
       enable = false,
     },
-    harpoon = {
-      -- https://github.com/ThePrimeagen/harpoon
-      enable = false,
-      key_mappings = function(harpoon)
-        vim.keymap.set("n", "<leader>a", function()
-          harpoon:list():append()
-        end, { desc = "Harpoon append" })
-        vim.keymap.set("n", "<leader>0", function()
-          harpoon.ui:toggle_quick_menu(harpoon:list())
-        end, { desc = "Harpoon UI" })
-        vim.keymap.set("n", "<leader>1", function()
-          harpoon:list():select(1)
-        end, { desc = "Harpoon 2" })
-        vim.keymap.set("n", "<leader>2", function()
-          harpoon:list():select(2)
-        end, { desc = "Harpoon 2" })
-        vim.keymap.set("n", "<leader>3", function()
-          harpoon:list():select(3)
-        end, { desc = "Harpoon 3" })
-        vim.keymap.set("n", "<leader>4", function()
-          harpoon:list():select(4)
-        end, { desc = "Harpoon 4" })
-      end,
-    },
     indent_blankline = {
       enable = false,
       enable_scope = true,
