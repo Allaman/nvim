8ad8a9c9af63e7ac534283fb11dc40529afe597b
Michael <michaeljohannpeter@gmail.com>
2024-03-26 17:01:45 +0100

    refactor!: gp.nvim config + add autocmds

--- lua/core/plugins/gp.lua
+++ lua/core/plugins/gp.lua
@@ -1,34 +1,67 @@
 local user_config = vim.g.config.plugins.gp
 
-local default_options = {
-  hooks = {
-    Translator = function(gp, params)
-      local agent = gp.get_command_agent()
-      local chat_system_prompt = "You are a Translator, please translate between German and English."
-      gp.cmd.ChatNew(params, agent.model, chat_system_prompt)
-    end,
+local default_config = {
+  enabled = false,
+  groups = {
+    ["<leader>i"] = { name = "AI" },
   },
+  keys = {
+    { "<leader>iv", "<cmd>GpChatNew vsplit<cr>", desc = "New Chat (vsplit)" },
+    { "<leader>it", "<cmd>GpChatToggle<cr>", desc = "Toggle last chat" },
+    { "<leader>if", "<cmd>GpChatFinder<cr>", desc = "Find chat" },
+    { "<leader>id", "<cmd>GpChatDelete<cr>", desc = "Delete current chat" },
+    { "<leader>ii", "<cmd>GpImage<cr>", desc = "Create an image" },
+    { "<leader>is", "<cmd>GpStop<cr>", desc = "Stop all running jobs and responses" },
+    { "<leader>il", "<cmd>GpTranslator vsplit<cr>", desc = "Translate" },
+  },
+  opts = {
+    hooks = {
+      Translator = function(gp, params)
+        local agent = gp.get_command_agent()
+        local chat_system_prompt = "You are a Translator, please translate between German and English."
+        gp.cmd.ChatNew(params, agent.model, chat_system_prompt)
+      end,
+    },
+  },
+  config_function = function(opts)
+    require("gp").setup(opts)
+
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+      pattern = { "*/gp/chats/*.md" },
+      callback = function(args)
+        vim.bo[args.buf].buflisted = false
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = args.buf, silent = true })
+        vim.opt_local.spell = false
+        vim.opt_local.list = false
+      end,
+      desc = "Configure gp.nvim chat buffer",
+    })
+    vim.api.nvim_create_autocmd({ "LspAttach" }, {
+      pattern = { "*/gp/chats/*.md" },
+      callback = function(args)
+        -- Delay the detachment by a short period to ensure the LSP client is fully attached
+        vim.defer_fn(function()
+          vim.lsp.buf_detach_client(args.buf, args.data.client_id)
+          vim.lsp.util.buf_clear_references(args.buf)
+          vim.opt.winbar = "" -- dropbar is triggered by LspAttach
+        end, 20) -- Delay in milliseconds
+      end,
+      desc = "Detach LSPs from gp.nvim chat buffer",
+    })
+  end,
 }
 
-local default_keys = {
-  { "<leader>iv", "<cmd>GpChatNew vsplit<cr>", desc = "New Chat (vsplit)" },
-  { "<leader>it", "<cmd>GpChatToggle<cr>", desc = "Toggle last chat" },
-  { "<leader>if", "<cmd>GpChatFinder<cr>", desc = "Find chat" },
-  { "<leader>id", "<cmd>GpChatDelete<cr>", desc = "Delete current chat" },
-  { "<leader>ii", "<cmd>GpImage<cr>", desc = "Create an image" },
-  { "<leader>is", "<cmd>GpStop<cr>", desc = "Stop all running jobs and responses" },
-  { "<leader>il", "<cmd>GpTranslator vsplit<cr>", desc = "Translate" },
-}
+local config = vim.tbl_deep_extend("force", default_config, user_config)
 
 return {
   {
     "robitx/gp.nvim",
     lazy = true,
-    enabled = user_config.enabled,
-    keys = vim.tbl_deep_extend("force", default_keys, (user_config.keys or {})),
-    opts = vim.tbl_deep_extend("force", default_options, (user_config.opts or {})),
+    enabled = config.enabled,
+    keys = config.keys,
+    opts = config.opts,
     config = function(_, opts)
-      require("gp").setup(opts)
+      config.config_function(opts)
     end,
   },
   -- which key integration
@@ -36,9 +69,7 @@ return {
     "folke/which-key.nvim",
     optional = true,
     opts = {
-      groups = {
-        ["<leader>i"] = { name = "AI" },
-      },
+      groups = config.groups,
     },
   },
 }
