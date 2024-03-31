b711cd9e7a278fe56220249a081c72e7ab92445b
Michael <michaeljohannpeter@gmail.com>
2024-03-31 19:11:15 +0200

    refactor: Remove obsolet hydra config

--- lua/core/plugins/hydra.lua
+++ lua/core/plugins/hydra.lua
@@ -3,34 +3,6 @@ local M = {
   lazy = true,
   dependencies = {
     "anuvyklack/keymap-layer.nvim",
-    {
-      -- for maximize mapping
-      "anuvyklack/windows.nvim",
-      lazy = true,
-      dependencies = {
-        "anuvyklack/middleclass",
-        "anuvyklack/animation.nvim",
-      },
-      opts = {
-        autowidth = { -- |windows.autowidth|
-          enable = false,
-          winwidth = 5, -- |windows.winwidth|
-          filetype = { -- |windows.autowidth.filetype|
-            help = 2,
-          },
-        },
-        ignore = { -- |windows.ignore|
-          buftype = { "quickfix" },
-          filetype = { "NvimTree", "neo-tree", "undotree", "NeogitStatus" },
-        },
-        animation = {
-          enable = false,
-          duration = 300,
-          fps = 30,
-          easing = "in_out_sine",
-        },
-      },
-    },
   },
 }
 
