17a0ab086d0224c73dbd7f32572a19a258e5a9db
Michael <michaeljohannpeter@gmail.com>
2024-04-09 20:36:43 +0200

    feat!: Replace mini.hipatterns w nvim-highlight-colors

---
+++ lua/core/plugins/highlight-colors.lua
@@ -0,0 +1,25 @@
+local user_config = vim.g.config.plugins.highlight_colors or {}
+
+local default_config = {
+  enabled = true,
+  opts = {},
+  keys = {
+    {
+      "<leader>tC",
+      function()
+        require("nvim-highlight-colors").toggle()
+      end,
+      desc = "Toggle highlight-colors",
+    },
+  },
+}
+
+local config = vim.tbl_deep_extend("force", default_config, user_config)
+
+return {
+  "brenoprata10/nvim-highlight-colors",
+  event = { "BufReadPre", "BufNewFile" },
+  enabled = config.enabled,
+  opts = config.opts,
+  keys = config.keys,
+}
--- lua/core/plugins/mini.lua
+++ lua/core/plugins/mini.lua
@@ -53,26 +53,6 @@ return {
   },
 
   {
-    "echasnovski/mini.hipatterns",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = function()
-      local hi = require("mini.hipatterns")
-      return {
-        highlighters = {
-          -- Highlight 'BUG', 'FIXME', 'HACK', 'TODO', 'NOTE'
-          bug = { pattern = "%f[%w]()BUG()%f[%W]", group = "MiniHipatternsFixme" },
-          fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-          hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-          todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-          note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-          hex_color = hi.gen_highlighter.hex_color(),
-          -- TODO: tailwind integration?
-        },
-      }
-    end,
-  },
-
-  {
     "echasnovski/mini.files",
     opts = {
       windows = {
