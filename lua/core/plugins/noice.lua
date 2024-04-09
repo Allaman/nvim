1a5612ac6c1f2f30ff04dbfd39164b3e5f6f392e
Michael <michaeljohannpeter@gmail.com>
2024-04-09 20:26:55 +0200

    refactor: noice config

--- lua/core/plugins/noice.lua
+++ lua/core/plugins/noice.lua
@@ -1,39 +1,46 @@
-local user_config = vim.g.config.plugins.noice
+local user_config = vim.g.config.plugins.noice or {}
 
-local default_options = {
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+local default_config = {
+  enabled = true,
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = false, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+    routes = {
+      { view = "notify", filter = { event = "msg_showmode" } }, -- show "recording macro"
+      --   { filter = { event = "msg_show", find = "written" }, opts = { skip = true } }, -- skip "write" message on save
     },
   },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = false, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = true, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
+  groups = {
+    ["<leader>n"] = { name = "Noice" },
   },
-  routes = {
-    { view = "notify", filter = { event = "msg_showmode" } }, -- show "recording macro"
-    --   { filter = { event = "msg_show", find = "written" }, opts = { skip = true } }, -- skip "writte" message on save
+  keys = {
+    { "<leader>nd", "<cmd>Noice dismiss<cr>", desc = "Dismiss all visible messages" },
+    { "<leader>nn", "<cmd>Noice<cr>", desc = "Open Noice" },
+    { "<leader>ne", "<cmd>Noice errors<cr>", desc = "Open Noice Errors" },
+    { "<leader>nt", "<cmd>Noice telescope<cr>", desc = "Open Noice with telescope" },
   },
 }
 
-local default_keys = {
-  { "<leader>nd", "<cmd>Noice dismiss<cr>", desc = "Dismiss all visible messages" },
-  { "<leader>nn", "<cmd>Noice<cr>", desc = "Open Noice" },
-  { "<leader>ne", "<cmd>Noice errors<cr>", desc = "Open Noice Errors" },
-  { "<leader>nt", "<cmd>Noice telescope<cr>", desc = "Open Noice with telescope" },
-}
+local config = vim.tbl_deep_extend("force", default_config, user_config)
 
 return {
   {
     "folke/noice.nvim",
-    enabled = user_config.enabled or true,
+    enabled = config.enabled,
     event = "VeryLazy",
     dependencies = {
       "MunifTanjim/nui.nvim",
@@ -44,17 +51,15 @@ return {
         },
       },
     },
-    keys = vim.tbl_deep_extend("force", default_keys, (user_config.keys or {})),
-    opts = vim.tbl_deep_extend("force", default_options, (user_config.opts or {})),
+    keys = config.keys,
+    opts = config.opts,
   },
   -- which key integration
   {
     "folke/which-key.nvim",
     optional = true,
     opts = {
-      groups = {
-        ["<leader>n"] = { name = "Noice" },
-      },
+      groups = config.groups,
     },
   },
 }
