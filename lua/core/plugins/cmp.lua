2c21d82d82e9caaadb85a2312770419def7582fe
Michael <michaeljohannpeter@gmail.com>
2024-05-25 12:27:05 +0200

    fix(cmp): Add CmdlineEnter to lazy event

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -1,6 +1,6 @@
 local M = {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
