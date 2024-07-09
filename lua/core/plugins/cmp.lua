1cf4a5f3ec43cfce1a78a2f2eb080488a5599362
Michael <michaeljohannpeter@gmail.com>
2024-07-09 20:39:51 +0200

    fix: Deprecated API

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -79,7 +79,7 @@ local M = {
     end
 
     local has_words_before = function()
-      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+      if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt" then
         return false
       end
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
