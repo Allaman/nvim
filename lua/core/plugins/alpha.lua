e99210ec12159f48ac933b678d6f53d182db2c88
Michael <michaeljohannpeter@gmail.com>
2024-07-10 22:43:00 +0200

    feat: Disable cursorline in alpha buffer

--- lua/core/plugins/alpha.lua
+++ lua/core/plugins/alpha.lua
@@ -15,6 +15,23 @@ return {
   },
   config = function()
     require("utils.functions").dashboard_autocmd(":Alpha | bd#")
+    -- Disable cursorline in alpha buffer
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "alpha" }, -- Add other buffer types as needed
+      callback = function()
+        vim.opt_local.cursorline = false
+      end,
+    })
+    -- Re-enable cursorline when entering other buffers
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = "*",
+      callback = function()
+        local ft = vim.bo.filetype
+        if not vim.tbl_contains({ "alpha" }, ft) then
+          vim.opt_local.cursorline = true
+        end
+      end,
+    })
     require("core.plugins.alpha.alpha")
   end,
 }
