64e4d5b6e67a9585098baa6cf45330f608658b39
Michael <michaeljohannpeter@gmail.com>
2024-07-11 00:40:35 +0200

    feat(alpha): Show a quote from zenquotes.io This feature is disabled because it causes a small lag from the http call when starting Neovim. You can enable it in your user config with plugins.alpha.quote = true

--- lua/core/plugins/alpha/alpha.lua
+++ lua/core/plugins/alpha/alpha.lua
@@ -5,6 +5,35 @@ if not status_ok then
   return
 end
 
+---Make a HTTP request to https://zenquotes.io to get a random quote
+---@return string
+local function get_quote()
+  local found_curl, curl = pcall(require, "plenary.curl")
+  if not found_curl then
+    error("plenary not found")
+    return ""
+  end
+
+  local response = curl.get("https://zenquotes.io/api/random", {
+    headers = {
+      ["User-Agent"] = "curl/7.68.0",
+    },
+  })
+
+  if response.status ~= 200 then
+    error("Http failed with " .. response.status, 1)
+    return ""
+  end
+
+  local json_data = vim.json.decode(response.body, {})
+  if json_data == {} or json_data == nil then
+    error("empty json from quotes API decoded")
+    return ""
+  end
+
+  return json_data[1].q
+end
+
 ---set a specific or a random header
 local function get_header()
   if utils.safe_nested_config(conf, "alpha", "header") then
@@ -34,6 +63,15 @@ dashboard.section.buttons.val = {
 for _, button in ipairs(dashboard.section.buttons.val) do
   button.opts.hl = "AlphaButtons"
   button.opts.hl_shortcut = "AlphaShortcut"
+end
+
+-- default is not enabled because of lag from the http call
+if conf.alpha.quote then
+  -- Do not run when a file is directly opened from the command line
+  -- and alpha is not even shown
+  if vim.fn.argc(-1) == 0 then
+    dashboard.section.footer.val = { get_quote() }
+  end
 end
 
 dashboard.section.header.opts.hl = "AlphaHeader"
