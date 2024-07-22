d47bdcff054f1a0e9d1dbb97569269579180194a
Michael <michaeljohannpeter@gmail.com>
2024-07-23 01:26:57 +0200

    feat!: Remove RELOAD function ... not used and lazy can do this

--- lua/utils/globals.lua
+++ lua/utils/globals.lua
@@ -1,20 +1,3 @@
-local ok, plenary_reload = pcall(require, "plenary.reload")
-if not ok then
-  RELOADER = require
-else
-  RELOADER = plenary_reload.reload_module
-end
-
-RELOAD = function(...)
-  return RELOADER(...)
-end
-
--- Reload a module
-R = function(name)
-  RELOAD(name)
-  return require(name)
-end
-
 -- Print the string representation of a Lua table
 P = function(v)
   vim.print(vim.inspect(v))
