ea8278a0da5c5f9531f9b9aa6e417fec19ce52bf
Michael <michaeljohannpeter@gmail.com>
2024-08-29 23:26:07 +0200

    feat: Add snippet for changlog

--- snippets/markdown.lua
+++ snippets/markdown.lua
@@ -83,4 +83,35 @@ return {
       }
     )
   ),
+  env.s(
+    { trig = "chlog", name = "Changelog", desc = "Keep a changelog template" },
+    env.fmt(
+      [[
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.1.1] - 2023-03-05
+
+### Added
+
+### Fixed
+
+### Changed
+
+### Removed
+
+[unreleased]: https://github.com/olivierlacan/keep-a-changelog/compare/v1.1.1...HEAD
+[0.0.3]: https://github.com/olivierlacan/keep-a-changelog/compare/v0.0.2...v0.0.3
+[0.0.2]: https://github.com/olivierlacan/keep-a-changelog/compare/v0.0.1...v0.0.2
+[0.0.1]: https://github.com/olivierlacan/keep-a-changelog/releases/tag/v0.0.1
+      ]],
+      {}
+    )
+  ),
 }
