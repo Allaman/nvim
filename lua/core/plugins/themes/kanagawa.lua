f8637141dc8fadc7900b1cad4ebfab01f0a9b8a5
Michael <michaeljohannpeter@gmail.com>
2024-11-24 23:13:19 +0100

    fix: Remove default options from kanagawa theme

--- lua/core/plugins/themes/kanagawa.lua
+++ lua/core/plugins/themes/kanagawa.lua
@@ -1,27 +1,3 @@
--- Default options:
 require("kanagawa").setup({
-  undercurl = true, -- enable undercurls
-  commentStyle = { italic = true },
-  functionStyle = {},
-  keywordStyle = { italic = true },
-  statementStyle = { bold = true },
-  typeStyle = {},
-  variablebuiltinStyle = { italic = true },
-  specialReturn = true, -- special highlight for the return keyword
-  specialException = true, -- special highlight for exception handling keywords
-  transparent = false, -- do not set background color
-  dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-  globalStatus = true, -- adjust window separators highlight for laststatus=3
-  terminalColors = true, -- define vim.g.terminal_color_{0,17}
-  colors = { -- add/modify theme and palette colors
-    palette = {},
-    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-  },
-  overrides = function(colors) -- add/modify highlights
-    return {}
-  end,
-  theme = "default", -- Load "default" theme or the experimental "light" theme
+  theme = vim.g.config.theme.kanagawa.variant,
 })
-
--- setup must be called before loading
-vim.cmd("colorscheme " .. vim.g.config.theme.kanagawa.variant)
