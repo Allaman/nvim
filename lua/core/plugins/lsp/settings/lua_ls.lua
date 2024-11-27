d38240b41bdeb96b5ff8b8961c36b55b41eb2f5a
Michael <michaeljohannpeter@gmail.com>
2024-11-27 23:33:00 +0100

    chore: Fix comment

--- lua/core/plugins/lsp/settings/lua_ls.lua
+++ lua/core/plugins/lsp/settings/lua_ls.lua
@@ -1,7 +1,7 @@
 -- https://github.com/sumneko/lua-language-server/blob/master/locale/en-us/setting.lua
 local opts = {
   format = {
-    enable = false, -- let null-ls handle the formatting
+    enable = false, -- let conform handle the formatting
   },
   diagnostics = { globals = { "vim" } },
   telemetry = { enable = false },
