f2f4eeb08b8220671a30cac7ae84c9f12ae37359
Michael <michaeljohannpeter@gmail.com>
2024-11-27 23:33:56 +0100

    feat: Refactor outline.nvim to new config structure

--- lua/core/plugins/outline.lua
+++ lua/core/plugins/outline.lua
@@ -1,15 +1,25 @@
-local M = {
-  "hedyhli/outline.nvim",
-  cmd = { "Outline", "OutlineOpen" },
+local user_config = vim.g.config.plugins.outline or {}
+
+local default_config = {
+  keys = {
+    { "<leader>to", "<cmd>Outline<cr>", desc = "Toggle Outline" },
+  },
   opts = {
     symbol_folding = {
       -- Depth past which nodes will be folded by default
       autofold_depth = 1,
     },
-  },
-  keys = {
-    { "<leader>to", "<cmd>Outline<cr>", desc = "Toggle Outline" },
+    guides = {
+      enabled = false,
+    },
   },
 }
 
-return M
+local config = vim.tbl_deep_extend("force", default_config, user_config)
+
+return {
+  "hedyhli/outline.nvim",
+  cmd = { "Outline", "OutlineOpen" },
+  opts = config.opts,
+  keys = config.keys,
+}
