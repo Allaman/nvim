8c78e7b8e81320f81393beb8e468e873a1415e56
Michael <michaeljohannpeter@gmail.com>
2024-12-04 19:16:26 +0100

    feat!: Rework spectre.nvim and make it optional

--- lua/core/plugins/spectre.lua
+++ lua/core/plugins/spectre.lua
@@ -1,34 +1,43 @@
-return {
-  {
-    "nvim-pack/nvim-spectre",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+local user_config = vim.g.config.plugins.spectre or {}
+
+local default_config = {
+  enabled = false,
+  keys = {
+    { "<leader>R", "", desc = "Search & Replace" },
+    {
+      "<leader>Rr",
+      function()
+        require("spectre").toggle()
+      end,
+      desc = "Toggle search and replace",
     },
-    enabled = vim.g.config.plugins.spectre.enable,
-    keys = {
-      { "<leader>R", "", desc = "Replace" },
-      {
-        "<leader>Rr",
-        function()
-          require("spectre").toggle()
-        end,
-        desc = "Toggle search and replace",
-      },
-      {
-        "<leader>Rw",
-        mode = { "v", "n" },
-        function()
-          require("spectre").open_visual({ select_word = true })
-        end,
-        desc = "Search word",
-      },
-      {
-        "<leader>Rf",
-        function()
-          require("spectre").open_file_search({ select_word = true })
-        end,
-        desc = "Search in current file",
-      },
+    {
+      "<leader>Rw",
+      mode = { "v", "n" },
+      function()
+        require("spectre").open_visual({ select_word = true })
+      end,
+      desc = "Search word",
+    },
+    {
+      "<leader>Rf",
+      function()
+        require("spectre").open_file_search({ select_word = true })
+      end,
+      desc = "Search in current file",
     },
   },
+  opts = {},
+}
+
+local config = vim.tbl_deep_extend("force", default_config, user_config)
+
+return {
+  "nvim-pack/nvim-spectre",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  enabled = config.enabled,
+  keys = config.keys,
+  opts = config.opts,
 }
