a15b9cdde0a1d019276782bfef0612a558f578d2
Michael <michaeljohannpeter@gmail.com>
2024-12-10 23:35:18 +0100

    feat!: Luapad config refactoring I want to be able to start Luapad always as Neovim gets more and more a Lua interpreter and also a text editor ;)

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -8,13 +8,6 @@ wk.add({
     end,
     desc = "Run buffer content",
   },
-  {
-    "<leader>cl",
-    function()
-      require("luapad").init()
-    end,
-    desc = "Luapad",
-  },
   { "<leader>ct", "<cmd>lua MiniTest.run_file()<cr>", desc = "MiniTest for current file" },
   { "<leader>cs", "<cmd>lua MiniTest.run()<cr>", desc = "Run MiniTest suite" },
 })
--- lua/core/plugins/luapad.lua
+++ lua/core/plugins/luapad.lua
@@ -1,6 +1,18 @@
-local M = {
-    "rafcamlet/nvim-luapad",
-    ft = "lua",
+local user_config = vim.g.config.plugins.luapad or {}
+
+local default_config = {
+  -- stylua: ignore start
+  keys = {
+    { "<leader>mL", function() require("luapad").init() end, desc = "Luapad", },
+  },
+  -- stylua: ignore end
+  opts = {},
 }
 
-return M
+local config = vim.tbl_deep_extend("force", default_config, user_config)
+
+return {
+  "rafcamlet/nvim-luapad",
+  keys = config.keys,
+  opts = config.opts,
+}
