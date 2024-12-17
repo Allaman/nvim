3692da2493d5a51349b83ef109ccd85c2e74ab12
Michael <michaeljohannpeter@gmail.com>
2024-12-17 22:54:35 +0100

    fix: Cleaner emoji.nvim config

--- lua/core/plugins/emoji.lua
+++ lua/core/plugins/emoji.lua
@@ -12,10 +12,6 @@ return {
     ft = "markdown",
     dependencies = "hrsh7th/nvim-cmp",
     opts = config.opts,
-    config = function(_, opts)
-      require("emoji").setup(opts)
-      require("telescope").load_extension("emoji")
-    end,
     dev = true,
   },
 
