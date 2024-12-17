14e3efe7ca5a6f88bde9724d8d388cba2a1f5976
Michael <michaeljohannpeter@gmail.com>
2024-12-17 22:54:09 +0100

    feat: Integrate supermaven in blink.cmp #93

--- lua/core/plugins/supermaven.lua
+++ lua/core/plugins/supermaven.lua
@@ -6,6 +6,9 @@ local default_config = {
   autostart = false,
   opts = {
     disable_inline_completion = true, -- disables inline completion for use with cmp
+    keymaps = {
+      accept_suggestion = nil, -- handled by nvim-cmp or blink.cmp
+    },
   },
   keys = {
     { "<leader>mC", "<cmd>SupermavenToggle<cr>", desc = "Toggle Supermaven" },
@@ -15,20 +18,42 @@ local default_config = {
 local config = vim.tbl_deep_extend("force", default_config, user_config)
 
 return {
-  "supermaven-inc/supermaven-nvim",
-  enabled = config.enabled,
-  event = "InsertEnter",
-  dependencies = "hrsh7th/nvim-cmp",
-  opts = config.opts,
-  keys = config.keys,
-  config = function(_, opts)
-    require("supermaven-nvim").setup(opts)
-    local api = require("supermaven-nvim.api")
-    if not config.autostart then
-      if api.is_running() then
-        api.stop()
-        return
+  {
+    "supermaven-inc/supermaven-nvim",
+    enabled = config.enabled,
+    -- event = "InsertEnter",
+    opts = config.opts,
+    keys = config.keys,
+    config = function(_, opts)
+      require("supermaven-nvim").setup(opts)
+      local api = require("supermaven-nvim.api")
+      if not config.autostart then
+        if api.is_running() then
+          api.stop()
+          return
+        end
       end
-    end
-  end,
+    end,
+  },
+  -- blink.cmp integration
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = {
+      "saghen/blink.compat",
+      "supermaven-inc/supermaven-nvim",
+    },
+    opts = {
+      sources = {
+        compat = { "supermaven" },
+        providers = {
+          supermaven = {
+            kind = "Supermaven",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
 }
