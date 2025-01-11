5ce07e4f6fe6952c536a9f1c88114b6e3debb932
Michael <github@mjpeter.de>
2025-01-11 22:08:38 +0100

    chore: Remove comments

--- lua/vnext/plugins/which-key.lua
+++ lua/vnext/plugins/which-key.lua
@@ -16,25 +16,15 @@ return {
       { "<leader>g", group = "Git" },
       { "<leader>l", group = "Code" },
       { "<leader>m", group = "Misc" },
-      -- { "<leader>q", group = "Quickfix" },
       { "<leader>R", group = "Replace" },
-      -- { "<leader>mS", group = "TreeSJ" },
       { "<leader>s", group = "Search" },
       { "<leader>t", group = "Toggles" },
       { "<leader>u", group = "UI" },
       { "<leader>w", group = "Window" },
-      -- { "<leader>z", group = "Spelling" },
-    },
-    win = {
-      -- title = false,
-      -- wo = {
-      --   winblend = 100, -- value between 0-100 (fully opaque - fully transparent)
-      -- },
     },
   },
   config = function(_, opts)
     vim.api.nvim_set_hl(0, "WhichKeyTitle", { bg = "#000000" }) -- HACK: only works if border is black
-    -- vim.api.nvim_set_hl(0, "WhichKeyFloat", { link = "WhichKeyBorder" })
     require("which-key").setup(opts)
   end,
 }
