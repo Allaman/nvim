4653b5db5b4179fd7dc4ecbb17d2235243b80703
Michael <github@mjpeter.de>
2025-01-12 14:07:29 +0100

    feat: Let neo-tree act as netrw init function copied from https://github.com/LazyVim/LazyVim

--- lua/vnext/plugins/neo-tree.lua
+++ lua/vnext/plugins/neo-tree.lua
@@ -5,6 +5,24 @@ return {
   keys = {
     { "<leader>fp", "<cmd>Neotree reveal toggle<cr>", desc = "Toggle Filetree" },
   },
+  init = function()
+    -- Load neo-tree when nvim is started with a directory
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+      desc = "Start Neo-tree with directory",
+      once = true,
+      callback = function()
+        if package.loaded["neo-tree"] then
+          return
+        else
+          local stats = vim.uv.fs_stat(vim.fn.argv(0))
+          if stats and stats.type == "directory" then
+            require("neo-tree")
+          end
+        end
+      end,
+    })
+  end,
   opts = {
     close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
     default_component_configs = {
@@ -86,9 +104,7 @@ return {
           [">"] = "next_source",
         },
       },
-      hijack_netrw_behavior = "disabled", -- netrw disabled, opening a directory opens neo-tree in whatever position is specified in window.position
-      -- "open_current",-- netrw disabled, opening a directory opens within the window like netrw would, regardless of window.position
-      -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+      hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens within the window like netrw would, regardless of window.position
     },
   },
   config = function(_, opts)
