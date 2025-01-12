e0d0e2cad69a90837bd298b892511b102243d8cb
Michael <github@mjpeter.de>
2025-01-12 17:05:15 +0100

    feat: Improve noice.nvim config

--- lua/vnext/plugins/ui.lua
+++ lua/vnext/plugins/ui.lua
@@ -43,22 +43,27 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    dependencies = {
-      -- {
-      --   "rcarriga/nvim-notify",
-      --   opts = {
-      --     top_down = true,
-      --   },
-      -- },
-    },
+    -- dependencies = {
+    --   { "rcarriga/nvim-notify" },
+    -- },
     keys = {
       { "<leader>n", "", desc = "Noice" },
       { "<leader>nn", "<cmd>Noice all<cr>", desc = "Open Noice" },
     },
     opts = {
+      cmdline = {
+        view = "cmdline", -- classic cmdline at the botton
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         lsp_doc_border = true, -- add a border to hover docs and signature help
       },
