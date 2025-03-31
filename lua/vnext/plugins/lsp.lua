4949f28d72e2df465b6f44c514599814dbb85432
Michael <github@mjpeter.de>
2025-03-31 22:32:55 +0200

    fix: clients.supports_method is now clients:supports:method

--- lua/vnext/plugins/lsp.lua
+++ lua/vnext/plugins/lsp.lua
@@ -145,7 +145,7 @@ return {
           --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
             local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
             vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
               buffer = event.buf,
