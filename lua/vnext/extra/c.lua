3ea5e775fc77fc6d97fb368a3dddbb8db95dcd47
Michael <github@mjpeter.de>
2025-04-21 14:07:56 +0200

    feat: Add c support (commented out)

---
+++ lua/vnext/extra/c.lua
@@ -0,0 +1,57 @@
+return {}
+-- local tools = {
+--   "clang-format",
+-- }
+-- local servers = {
+--   clangd = {
+--     cmd = {
+--       "clangd",
+--       "--background-index",
+--       "--clang-tidy",
+--       "--header-insertion=iwyu",
+--       "--completion-style=detailed",
+--       "--function-arg-placeholders",
+--       "--fallback-style=llvm",
+--     },
+--     init_options = {
+--       usePlaceholders = true,
+--       completeUnimported = true,
+--       clangdFileStatus = true,
+--     },
+--     root_dir = function(fname)
+--       return require("lspconfig.util").root_pattern(
+--         "Makefile",
+--         "configure.ac",
+--         "configure.in",
+--         "config.h.in",
+--         "meson.build",
+--         "meson_options.txt",
+--         "build.ninja"
+--       )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname) or require(
+--         "lspconfig.util"
+--       ).find_git_ancestor(fname)
+--     end,
+--   },
+-- }
+--
+-- return {
+--   {
+--     "stevearc/conform.nvim",
+--     opts = {
+--       formatters_by_ft = {
+--         c = { "clang-format" },
+--       },
+--     },
+--   },
+--
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = ":TSUpdate",
+--     event = { "BufReadPost", "BufNewFile" },
+--     opts = {
+--       ensure_installed = {
+--         "c",
+--       },
+--     },
+--   },
+-- }
