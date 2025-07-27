a76a30aad43e84dfcba480e6a12654c75232c227
Michael <github@mjpeter.de>
2025-07-27 19:36:29 +0200

    feat!: oil is not default file explorer

--- lua/vnext/plugins/file-manager.lua
+++ lua/vnext/plugins/file-manager.lua
@@ -5,7 +5,6 @@ return {
       { "-", "<cmd>Oil<cr>", desc = "Open Oil" },
     },
     opts = {
-      default_file_explorer = true,
       delete_to_trash = true,
       skip_confirm_for_simple_edits = true,
     },
