c24770ad51570bb3e62174e2a977a4f59331ebb1
Michael <github@mjpeter.de>
2025-08-03 16:39:05 +0200

    feat!: Remove gitlinker.nvim (snacks does the job too)

--- lua/vnext/plugins/git.lua
+++ lua/vnext/plugins/git.lua
@@ -64,11 +64,4 @@ return {
       require("gitsigns").setup(opts)
     end,
   },
-  {
-    "linrongbin16/gitlinker.nvim",
-    keys = {
-      { "<leader>gc", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
-      { "<leader>go", "<cmd>GitLink! blame<cr>", mode = { "n", "v" }, desc = "Open git link" },
-    },
-  },
 }
