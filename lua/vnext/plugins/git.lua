b5b0a4a7fa5bb0c44b1e2b6e0d4db0e31536612d
Michael <github@mjpeter.de>
2025-09-20 12:05:35 +0200

    feat!: Replace some snacks commands with fugitive

--- lua/vnext/plugins/git.lua
+++ lua/vnext/plugins/git.lua
@@ -3,14 +3,44 @@ return {
     "tpope/vim-fugitive",
     cmd = "G",
     config = function()
+      local fugitive = vim.api.nvim_create_augroup("Fugitive", { clear = true })
       vim.api.nvim_create_autocmd("FileType", {
+        group = fugitive,
         pattern = { "fugitiveblame", "fugitive" },
         callback = function()
           vim.api.nvim_buf_set_keymap(0, "n", "q", "gq", {})
         end,
         desc = "Close fugitive with q instead of gq",
       })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = fugitive,
+        pattern = { "git" },
+        callback = function()
+          vim.opt_local.foldmethod = "syntax"
+          vim.opt_local.foldlevel = 0
+          vim.opt_local.foldenable = true
+        end,
+      })
+
+      vim.api.nvim_create_user_command("Gcn", function(opts)
+        local branch_name = opts.args
+        if branch_name == "" then
+          vim.api.nvim_err_writeln("Error: Branch name is required")
+          return
+        end
+        vim.cmd("Git checkout -b " .. branch_name)
+      end, {
+        nargs = 1,
+        desc = "Create and checkout new git branch",
+      })
     end,
+    keys = {
+      { "<leader>gg", "<cmd>G<cr>", desc = "G" },
+      { "<leader>gl", "<cmd>GcLog<cr>", desc = "Difflog" },
+      { "<leader>gD", "<cmd>Gvdiffsplit<cr>", desc = "Diffsplit" },
+      { "<leader>gf", "<cmd>0GcLog<cr>", desc = "Difflog file" },
+    },
   },
   {
     "lewis6991/gitsigns.nvim",
@@ -53,7 +83,7 @@ return {
       map("n", "<leader>gR", gs.reset_buffer, { desc = "Reset buffer" })
       map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
       map("n", "<leader>gb", gs.toggle_current_line_blame, { desc = "Toggle Blame line" })
-      map("n", "<leader>gD", gs.diffthis, {desc="Diff"})
+      -- map("n", "<leader>gD", gs.diffthis, {desc="Diff"})
       map("n", "<leader>gd", gs.preview_hunk_inline, { desc = "Show deleted" })
       end,
       -- stylua: ignore end
