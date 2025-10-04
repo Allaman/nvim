822b737be74ad7a7fcc918e21309957159417138
Michael <github@mjpeter.de>
2025-10-05 00:10:00 +0200

    feat!: Replace lua_ls with emmylua_ls

--- lua/vnext/plugins/lsp.lua
+++ lua/vnext/plugins/lsp.lua
@@ -42,43 +42,12 @@ return {
             semanticTokens = true,
           },
         },
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
-            format = {
-              enable = false, -- let conform handle the formatting
-            },
-            diagnostics = { globals = { "vim" } },
-            telemetry = { enable = false },
-            hint = { enable = true },
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
               },
-              codeLens = {
-                enable = true,
-              },
-              doc = {
-                privateName = { "^_" },
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
+        emmylua_ls = {},
         marksman = {},
         pyright = {},
         templ = {},
