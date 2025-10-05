587bea4c1fd1b72149796bdad444bb50017d3ea2
Michael <github@mjpeter.de>
2025-10-05 12:20:23 +0200

    feat!: Replace oil.nvim and neo-tree.nvim with fyler.nvim Fyler combines both plugins, though not as configurable (yet): File manipulating in a buffer as oil.nvim and filetree like neo-tree.nvim

--- lua/vnext/plugins/statusline.lua
+++ lua/vnext/plugins/statusline.lua
@@ -71,7 +71,7 @@ return {
     extensions = { "lazy", "quickfix", "neo-tree" },
     options = {
       disabled_filetypes = {
-        statusline = { "neo-tree", "Outline", "snacks_picker_list" },
+        statusline = { "neo-tree", "Outline", "snacks_picker_list", "Fyler" },
       },
     },
     sections = {
