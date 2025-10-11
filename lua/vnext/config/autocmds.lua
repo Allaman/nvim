a14c9ee62f981ac144fd6987e803a34b50b9a55c
Michael <github@mjpeter.de>
2025-10-11 02:32:27 +0200

    chore: Use local require

--- lua/vnext/config/autocmds.lua
+++ lua/vnext/config/autocmds.lua
@@ -7,7 +7,7 @@ api.nvim_create_autocmd("TextYankPost", {
   desc = "highlight on yank",
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
+api.nvim_create_autocmd("BufEnter", {
   callback = function()
     vim.opt.formatoptions:remove({ "c", "r", "o" })
   end,
@@ -16,10 +16,10 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 api.nvim_create_autocmd("BufReadPost", {
   callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
+    local mark = api.nvim_buf_get_mark(0, '"')
+    local lcount = api.nvim_buf_line_count(0)
     if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      pcall(api.nvim_win_set_cursor, 0, mark)
     end
   end,
   desc = "go to last loc when opening a buffer",
@@ -51,7 +51,7 @@ api.nvim_create_autocmd("FileType", {
 })
 
 local chezmoi_path = vim.fn.resolve(vim.fn.expand("~/.local/share/chezmoi"))
-vim.api.nvim_create_autocmd("BufWritePost", {
+api.nvim_create_autocmd("BufWritePost", {
   pattern = {
     chezmoi_path .. "/**/*", -- files in subdirectories
   },
