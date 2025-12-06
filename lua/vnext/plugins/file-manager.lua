341024146a2c3b9f9a68e6508275efbcd930a6a3
Michael <github@mjpeter.de>
2025-12-06 16:58:51 +0100

    feat: Update fyler.nvim config

--- lua/vnext/plugins/file-manager.lua
+++ lua/vnext/plugins/file-manager.lua
@@ -1,7 +1,7 @@
 return {
   {
     "A7Lavinraj/fyler.nvim",
-    branch = "stable",
+    dev = true,
     keys = {
       { "-", "<cmd>lua require('fyler').toggle({kind = 'float'})<cr>", desc = "Fyler" },
       { "<leader>fp", "<cmd>lua require('fyler').toggle({kind = 'split_left_most'})<cr>", desc = "Toggle Filetree" },
@@ -20,7 +20,7 @@ return {
           local arg1 = args[1]
           -- Open fyler if one argument and that's a directory
           if #args == 1 and arg1 then
-            local stats = vim.uv.fs_stat(vim.fn.argv(0) --[[@as string]])
+            local stats = vim.uv.fs_stat(arg1 --[[@as string]])
             if stats and stats.type == "directory" then
               require("fyler").open()
             end
@@ -29,10 +29,19 @@ return {
       })
     end,
     opts = {
-      close_on_select = false,
-      default_explorer = true,
-      indentscope = {
-        enabled = false,
+      views = {
+        finder = {
+          close_on_select = false,
+          default_explorer = true,
+          delete_to_trash = true,
+          indentscope = {
+            enabled = false,
+          },
+          mappings = {
+            ["C-v"] = "SelectVSplit",
+            ["C-s"] = "SelectSplit",
+          },
+        },
       },
     },
   },
