c90b1f6b972c54562d51f2b08a45abeb962766a8
Michael <github@mjpeter.de>
2026-03-31 02:02:02 +0200

    fix(bufferline): Update config

--- lua/vnext/plugins/ui.lua
+++ lua/vnext/plugins/ui.lua
@@ -129,6 +129,8 @@ return {
             text = "File Explorer",
           },
         },
+        show_buffer_close_icons = false,
+        always_show_bufferline = false,
       },
     },
     keys = {
