9e6cc67cca996543350df14f60bbb86813984693
Michael <github@mjpeter.de>
2026-07-05 23:59:15 +0200

    feat!: Configure Navigator for herdr instead of tmux

--- lua/vnext/plugins/navigator.lua
+++ lua/vnext/plugins/navigator.lua
@@ -1,6 +1,12 @@
 return {
   "numToStr/Navigator.nvim",
   opts = {},
+  init = function()
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
+  config = function()
+    dofile(vim.fn.expand("~/.config/herdr/plugins/github/vim-herdr-navigation-a8bf42123d81/editor/nvim.lua"))
+  end,
   keys = {
     { "<C-h>", "<cmd>lua require('Navigator').left()<CR>" },
     { "<C-k>", "<cmd>lua require('Navigator').up()<CR>" },
