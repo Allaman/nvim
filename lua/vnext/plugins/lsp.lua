824c1477fe12b631ec7140d8a417fe19941c597a
Michael <github@mjpeter.de>
2026-08-29 03:32:51 +0200

    fix: Update teraform-ls

--- lua/vnext/plugins/lsp.lua
+++ lua/vnext/plugins/lsp.lua
@@ -116,7 +116,7 @@ return {
         templ = {},
         terraformls = {
           cmd = {
-            "/Users/michael/.vscode/extensions/hashicorp.terraform-2.39.4-darwin-arm64/bin/terraform-ls",
+            "/Users/michael/.vscode/extensions/hashicorp.terraform-2.40.0-darwin-arm64/bin/terraform-ls",
             "serve",
           },
           filetypes = { "terraform", "terraform-vars", "tf" },
